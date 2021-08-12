3b4df05564f80a8bd7286bf33dd77b8bc50f32de
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2021-08-12 22:25:20 +0900

    e: tle profile

--- 213/e.pl
+++ 213/e.pl
@@ -5,9 +5,10 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
+use Time::HiRes qw( tv_interval gettimeofday );
 
 # TIME PASSED
-# 🍅🍅🍅🍅
+# 🍅🍅🍅🍅🍅🍅🍅
 #
 
 my @NEIGHBORS = (
@@ -16,6 +17,36 @@ my @NEIGHBORS = (
     [ -1, 0 ],
     [ 0, -1 ],
 );
+
+my @PUNCH_NEIGHBORS = (
+    [ -1, -2 ],
+    [ 0, -2 ],
+    [ 1, -2 ],
+
+    [ -2, -1 ],
+    [ -1, -1 ],
+    [ 0, -1 ],
+    [ 1, -1 ],
+    [ 2, -1 ],
+
+    [ -2, 0 ],
+    [ -1, 0 ],
+    [ 1, 0 ],
+    [ 2, 0 ],
+
+    [ -2, 1 ],
+    [ -1, 1 ],
+    [ 0, 1 ],
+    [ 1, 1 ],
+    [ 2, 1 ],
+
+    [ -1, 2 ],
+    [ 0, 2 ],
+    [ 1, 2 ],
+);
+
+my %elapsed = ( );
+
 
 my( $h, $w ) = split m{\s}, <>;
 chomp( my @lines = <> );
@@ -31,17 +62,20 @@ initialize_cost( $_, $max_cost )
     for map { my $x = $_; map { [ $x, $_ ] } 0 .. $h - 1 } 0 .. $w - 1;
 set_cost( [ 0, 0 ], 0 );
 
-unshift @deque, [ [ 0, 0 ], [ 0, 0 ] ];
+unshift @deque, [ 0, 0 ];
 
+my $started = [ gettimeofday ];
 while ( @deque ) {
-    my( $current, $parent ) = @{ shift @deque };
+    my $current = shift @deque;
 
     next
-        if is_visited( $current );
+        if $visited{ $current->[0] }{ $current->[1] };#is_visited( $current );
 
     mark_visit( $current );
 
 #warn "### ($current->[0], $current->[1]) <- ($parent->[0], $parent->[1])", ": ", cost_of( $current );
+
+    my $start = [ gettimeofday ];
 
     for my $candidate ( @NEIGHBORS ) {
         my $neighbor = add_coord( $current, $candidate );
@@ -50,44 +84,70 @@ while ( @deque ) {
             unless is_in_range( $neighbor );
 
         next
-            if is_visited( $neighbor );
+            if $visited{ $neighbor->[0] }{ $neighbor->[1] };# is_visited( $neighbor );
 
         my $is_wall = tile_of( $neighbor ) eq "#";
 
         set_cost( $neighbor, cost_of( $current ) + $is_wall );
 
         if ( $is_wall ) {
-            push @deque, [ $neighbor, $current ];
+            push @deque, $neighbor;
         }
 
         if ( !$is_wall ) {
-            unshift @deque, [ $neighbor, $current ];
+            unshift @deque, $neighbor;
         }
     }
 
-    for my $delta_x ( -2 .. 2 ) {
-        for my $delta_y ( -2 .. 2 ) {
-            next
-                if abs( $delta_x * $delta_y ) == 4;
+    $elapsed{neighbor} += tv_interval( $start );
 
-            my $candidate = add_coord( $current, [ $delta_x, $delta_y ] );
+    $start = [ gettimeofday ];
 
-            next
-                unless is_in_range( $candidate );
+    for my $candidate ( @PUNCH_NEIGHBORS ) {
+        my $neighbor = add_coord( $current, $candidate );
 
-            my $neighbor = $candidate;
+        next
+            unless is_in_range( $neighbor );
 
-            set_cost( $neighbor, cost_of( $current ) + 1 );
-            push @deque, [ $neighbor, $current ];
-        }
+        next
+            if is_visited( $neighbor );
+
+        set_cost( $neighbor, cost_of( $current ) + 1 );
+        push @deque, $neighbor;
     }
+
+    $elapsed{punch} += tv_interval( $start );
 }
 
 #warn Dumper \%visited;
 #warn Dumper \%cost;
 say $cost{ $w - 1 }{ $h - 1 };
 
+warn "total elapsed: ", tv_interval( $started );
+warn Dumper \%elapsed;
+
+
 exit;
+
+sub deque_push {
+    my $start = [ gettimeofday ];
+    push @deque, shift;
+    $elapsed{push} += tv_interval( $start );
+}
+
+sub deque_unshift {
+    my $start = [ gettimeofday ];
+    unshift @deque, shift;
+    $elapsed{unshift} += tv_interval( $start );
+}
+
+sub deque_shift {
+    my $start = [ gettimeofday ];
+    my $item = shift @deque;
+    $elapsed{shift} += tv_interval( $start );
+
+    return $item;
+}
 
 sub initialize_cost {
     my $coord = shift;
@@ -135,8 +195,8 @@ sub cost_of {
     my $coord = shift
         or return 0;
     my( $x, $y ) = @{ $coord };
-    $cost{ $x } = { }
-        unless exists $cost{ $x };
+    #$cost{ $x } = { }
+    #        unless exists $cost{ $x };
     return $cost{ $x }{ $y } // 0;
 }
 
@@ -149,4 +209,10 @@ sub is_in_range {
 sub add_coord {
     my( $a, $b ) = @_;
     return [ $a->[0] + $b->[0], $a->[1] + $b->[1] ];
+}
+
+package Deque;
+{
+    class Deque {
+    }
 }

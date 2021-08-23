54eb943dd7b892479b3ec3be407f6a519e827e00
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2021-08-23 21:38:29 +0900

    215.c: ac...

--- 215/c.pl
+++ 215/c.pl
@@ -9,26 +9,53 @@ use Data::Dumper;
 chomp( my $line = <> );
 my( $s, $k ) = split m{\s}, $line;
 
-my @chars = sort split m{}, $s;
+my $worker = OrderPattern->new( $s );
+$worker->list;
 
-my @words = ( ( join q{}, @chars ), recursive( @chars ) );
-warn Dumper \@words;
-
-#my $previous = q{};
-#@words = grep { my $r = $_ ne $previous; $previous = $_; $r } sort @words;
+my $previous = q{};
+my @words = grep { my $is_same = $_ ne $previous; $previous = $_; $is_same }
+            sort @{ $worker->{result} };
 
 say $words[ $k - 1 ];
 
 exit;
 
-sub recursive {
-    my @chars = @_;
-    return ( $chars[0] )
-        if @chars == 1;
-    return ( "$chars[0]$chars[1]", "$chars[1]$chars[0]" )
-        if @chars == 2;
-    return (
-        $chars[0] . recursive( @chars[ 1 .. $#chars ] ),
-        recursive( @chars[ 1 .. $#chars ] ) . $chars[0],
-    );
+package OrderPattern;
+
+sub new {
+    my $class = shift;
+    my $string = shift;
+
+    return bless { string => [ split m{}, $string ] }, $class;
+}
+
+sub result {
+    my $self = shift;
+    return $self->{result};
+}
+
+sub list {
+    my $self = shift;
+    $self->{chars} = [ ];
+    $self->{size} = @{ $self->{string} };
+    $self->{result} = [ ];
+    return $self->retrieve_all_patterns( ( 1 << $self->{size} ) - 1 );
+}
+
+sub retrieve_all_patterns {
+    my $self = shift;
+    my $usable_bitmap = shift;
+
+    if ( $usable_bitmap == 0 ) {
+        push @{ $self->{result} }, join q{}, @{ $self->{chars} };
+        return;
+    }
+
+    for my $i ( 0 .. ( $self->{size} - 1 ) ) {
+        if ( $usable_bitmap & ( 1 << $i ) ) {
+            push @{ $self->{chars} }, $self->{string}[ $i ];
+            $self->retrieve_all_patterns( $usable_bitmap ^ ( 1 << $i ) );
+            pop @{ $self->{chars} };
+        }
+    }
 }

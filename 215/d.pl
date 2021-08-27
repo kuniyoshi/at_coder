dc71a8c9c49d555d76391d689ae1831bbfa340d4
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2021-08-27 22:39:39 +0900

    215.d: AC

--- 215/d.pl
+++ 215/d.pl
@@ -5,33 +5,43 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
+use List::Util qw( max );
 
 chomp( my $head_line = <> );
 chomp( my $body_line = <> );
 my( $n, $m ) = split m{\s}, $head_line;
+my @a = split m{\s}, $body_line;
 
-my @result = ( 1 ) x ( $m + 1 );
-$result[0] = 0;
+my @b = ( );
+$b[ $_ - 1 ]++
+    for @a;
 
-chomp( my @lines = `factor $body_line` );
+my $max = max( @a ) // 0;
 
-for my $line ( @lines ) {
-    my @factors = split m{\s}, $line;
-    shift @factors;
+my @result = ( 1 ) x $m;
 
-    for my $prime_factor ( @factors ) {
+for ( my $i = 2; $i <= $m; ++$i ) {
+    my $has;
+
+    for ( my $j = $i; $j <= $max; $j += $i ) {
         next
-            unless $result[ $prime_factor ];
+            unless $b[ $j - 1 ];
 
-        for ( my $i = $prime_factor; $i <= @result; $i += $prime_factor ) {
-            $result[ $i ] = 0
-        }
+        $has++;
+        last;
+    }
+
+    next
+        unless $has;
+
+    for ( my $j = $i; $j <= $m; $j += $i ) {
+        $result[ $j - 1 ] = 0;
     }
 }
 
 say scalar grep { $_ } @result;
 say
-    for grep { $result[ $_ ] }
-        ( 1 .. $#result );
+    for grep { $result[ $_ - 1 ] }
+        ( 1 .. $m );
 
 exit;

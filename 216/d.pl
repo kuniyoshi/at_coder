d9dd74f4a39a888f214019e1602affc3adbcd6bb
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2021-08-30 23:20:12 +0900

    216.d: WA, TLE

--- 216/d.pl
+++ 216/d.pl
@@ -6,5 +6,35 @@ use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
 
+chomp( my $line = <> );
+my( $n, $m ) = split m{\s}, $line;
+my @k = ( );
+
+for my $i ( 1 .. $m ) {
+    chomp( my $count = <> );
+    chomp( my $numbers = <> );
+    push @k, [ split m{\s}, $numbers ];
+}
+
+while ( @k ) {
+    my @sorted = sort { @{ $b } <=> @{ $a } } @k;
+    my $number = $sorted[0][0];
+    my $pair;
+    for ( my $i = 1; $i < @sorted; ++$i ) {
+        next
+            unless $sorted[ $i ][0] == $number;
+        $pair = $i;
+        last;
+    }
+    last
+        unless defined $pair;
+    shift @{ $sorted[0] };
+    shift @{ $sorted[ $pair ] };
+    my $both_contains = @{ $sorted[0] } && @{ $sorted[ $pair ] };
+    @k = grep { @{ $_ } } @k
+        unless $both_contains;
+}
+
+say @k ? "No" : "Yes";
 
 exit;

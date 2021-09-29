22f9fb3790dccd4e4b25169bbdb147a42b881818
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2021-09-29 23:45:55 +0900

    220.d: WA, TLE

--- 220/d.pl
+++ 220/d.pl
@@ -10,18 +10,31 @@ chomp( my $n = <> );
 chomp( my $line = <> );
 my @a = split m{\s}, $line;
 
-my @f = ( 0 ) x 10;
-my @g = ( 0 ) x 10;
+my @patterns = ( undef ) x 10;
+$patterns[ shift @a ]++;
 
-for my $i ( 1 .. $n - 1 ) {
-    my $x = shift @a;
+while ( @a ) {
     my $y = shift @a;
-    my $f = ( $x + $y ) % 10;
-    my $g = ( $x * $y ) % 10;
-    $
+
+    my @nexts = ( undef ) x 10;
+
+    for ( my $i = 0; $i < @patterns; ++$i ) {
+        next
+            unless defined $patterns[ $i ];
+
+        my $x = $i;
+        my $f = ( $x + $y ) % 10;
+        my $g = ( $x * $y ) % 10;
+        $nexts[ $f ] = ( $nexts[ $f ] // 0 ) + $patterns[ $i ];
+        $nexts[ $g ] = ( $nexts[ $g ] // 0 ) + $patterns[ $i ];
+    }
+
+    for ( my $i = 0; $i < 10; ++$i ) {
+        $patterns[ $i ] = $nexts[ $i ];
+    }
 }
 
-say $f[ $_ ] % 998244353 + $g[ $_ ] % 998244353
-    for 0 .. 9;
+say $_ % 998244353
+    for map { $_ // 0 } @patterns;
 
 exit;

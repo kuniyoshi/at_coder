69de9d3a3a35ee940764903771ac63b35f91f4dc
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2021-10-11 23:10:15 +0900

    222.d: O(N^3)

--- 222/d.pl
+++ 222/d.pl
@@ -5,6 +5,7 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
+use List::Util qw( sum );
 
 chomp( my $n = <> );
 chomp( my $line_a = <> );
@@ -12,30 +13,30 @@ chomp( my $line_b = <> );
 my @a = ( 0, split m{\s}, $line_a );
 my @b = ( 0, split m{\s}, $line_b );
 
-my @previous = ( $a[0], $b[0] );
-my @current = ( 0, 0 );
-my $result = 1;
+my @dp;
+$dp[ $_ ] = [ ( 0 ) x 3001 ]
+    for 0 .. $n;
+$dp[0][0] = 1;
 
-for my $i ( 2 .. $n ) { # die
-    $current[0] = $a[ $i - 1 ];
-    $current[1] = $b[ $i - 1 ];
-
-    my $increment = 0;
-
-    for my $j ( $previous[0] .. $previous[1] ) {
-        my $candidate = $current[1] - $current[0] - $j + 2;
-        warn "\$i: $i, \$j: $j, \$candidate: $candidate";
-        die
-            if $candidate < 0;
-        $increment += $candidate;
+for my $i ( 1 .. $n ) {
+    for my $j ( 0 .. 3000 ) {
+        if ( is_in( $j, $a[ $i ], $b[ $i ] ) ) {
+            my $sum = sum( @{ $dp[ $i - 1 ] }[ 0 .. $j ] );
+            $dp[ $i ][ $j ] = $sum % 998244353;
+        }
+        else {
+            $dp[ $i ][ $j ] = 0;
+        }
     }
-
-    $result = ( $result * $increment ) % 998244353;
-
-    $previous[0] = $previous[1] > $current[0] ? $previous[1] : $current[0];
-    $previous[1] = $current[1];
 }
 
-say $result;
+say sum( @{ $dp[ $n ] } ) % 998244353;
 
 exit;
+
+sub is_in {
+    my $target = shift;
+    my $min = shift;
+    my $max = shift;
+    return $target >= $min && $target <= $max;
+}

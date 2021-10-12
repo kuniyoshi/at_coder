5a6a74c8fdcf1e295ce324ac3b0b18fbc52d6363
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2021-10-12 23:56:43 +0900

    222.d: TLE

--- 222/d.pl
+++ 222/d.pl
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
-use List::Util qw( sum );
+use List::Util qw( sum max );
 
 chomp( my $n = <> );
 chomp( my $line_a = <> );
@@ -13,24 +13,33 @@ chomp( my $line_b = <> );
 my @a = ( 0, split m{\s}, $line_a );
 my @b = ( 0, split m{\s}, $line_b );
 
+my $m = max( @b );
+
 my @dp;
-$dp[ $_ ] = [ ( 0 ) x 3001 ]
+$dp[ $_ ] = [ ( 0 ) x ( $m + 1 ) ]
     for 0 .. $n;
-$dp[0][0] = 1;
+$dp[0][ $_ ] = 1
+    for 0 .. $m;
 
 for my $i ( 1 .. $n ) {
-    for my $j ( 0 .. 3000 ) {
+    if ( is_in( 0, $a[ $i ], $b[ $i ] ) ) {
+        $dp[ $i ][0] = $dp[ $i - 1 ][0];
+    }
+    else {
+        $dp[ $i ][0] = 0;
+    }
+
+    for my $j ( 1 .. $m ) {
         if ( is_in( $j, $a[ $i ], $b[ $i ] ) ) {
-            my $sum = sum( @{ $dp[ $i - 1 ] }[ 0 .. $j ] );
-            $dp[ $i ][ $j ] = $sum % 998244353;
+            $dp[ $i ][ $j ] = ( $dp[ $i - 1 ][ $j ] + $dp[ $i ][ $j - 1 ] ) % 998244353;
         }
         else {
-            $dp[ $i ][ $j ] = 0;
+            $dp[ $i ][ $j ] = $dp[ $i ][ $j - 1 ];
         }
     }
 }
 
-say sum( @{ $dp[ $n ] } ) % 998244353;
+say $dp[ $n ][ $m ];
 
 exit;
 

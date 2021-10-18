f34443271177e35e92888353c1a6e11606ab2a74
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2021-10-18 23:48:31 +0900

    223.d: should be TLE

--- 223/d.pl
+++ 223/d.pl
@@ -6,5 +6,44 @@ use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
 
+chomp( my $head = <> );
+my( $n, $m ) = split m{\s}, $head;
+my @edges = map { chomp; [ split m{\s} ] }
+            map { <> }
+            1 .. $m;
+
+my %graph;
+$graph{ $_ } = undef
+    for 1 .. $m;
+
+for my $edge_ref ( @edges ) {
+    my( $from, $to ) = @{ $edge_ref };
+    push @{ $graph{ $to } }, $from;
+}
+
+my @s;
+
+while ( grep { !defined $graph{ $_ } } keys %graph ) {
+    my( $u ) = sort { $a <=> $b }
+               grep { !defined $graph{ $_ } }
+               keys %graph;
+    push @s, $u;
+    delete $graph{ $u };
+
+    for my $key ( keys %graph ) {
+        next
+            unless defined $graph{ $key };
+        $graph{ $key } = [ grep { $_ != $u } @{ $graph{ $key } } ];
+        undef $graph{ $key }
+            unless @{ $graph{ $key } };
+    }
+}
+
+if ( %graph ) {
+    say -1;
+    exit;
+}
+
+say join q{ }, @s;
 
 exit;

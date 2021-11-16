31ee3f7ed0f1eb9d5c50384ff01cc1c8b236ac5a
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2021-11-16 21:58:38 +0900

    227.c: TLE

--- 227/c.pl
+++ 227/c.pl
@@ -6,5 +6,20 @@ use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
 
+chomp( my $n = <> );
+
+my $count = 0;
+
+use integer;
+
+for ( my $a = 1; $a * $a * $a < $n; ++$a ) {
+    for ( my $b = $a; $a * $b * $b <= $n; ++$b ) {
+        my $max = $n / ( $a * $b );
+        my $patterns = $max - $b + 1;
+        $count = $count + $patterns;
+    }
+}
+
+say $count;
 
 exit;

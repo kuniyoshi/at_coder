c26cdd56e1381794343e8648cd626b3e7d8d483c
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2021-11-17 19:08:42 +0900

    227.d: wip

--- 227/d.pl
+++ 227/d.pl
@@ -5,6 +5,47 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
+use List::Util qw( sum );
 
+my( $n, $k ) = do { chomp( my $l = <> ); split m{\s}, $l };
+my @a = do { chomp( my $l = <> ); split m{\s}, $l };
+
+use integer;
+
+my $left = 1;
+my $right = ( sum @a ) / $k;
+my $center = 1;
+my $answer = 1;
+
+while ( $left < $right ) {
+    $center = $left + ( $right - $left ) / 2;
+    warn "### ($left, $center, $right)";
+    warn "--- is_true: ", is_true( $center );
+
+    if ( is_true( $center ) ) {
+        $right = $center;
+        $answer = $center;
+    }
+    else {
+        $left = $center + 1;
+    }
+}
+
+say $answer;
 
 exit;
+
+sub is_true {
+    my $x = shift;
+
+    my $max = $x * $k;
+    my $sum = 0;
+
+    for my $a ( @a ) {
+        $sum = $sum + ( $a > $k ? $k : $a );
+        return 1
+            if $sum >= $max;
+    }
+
+    return;
+}

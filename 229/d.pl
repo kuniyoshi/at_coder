4fc2a6d6b325aecd3285b6c8b24b23560c091956
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2021-11-30 22:12:36 +0900

    229.d: wip

--- 229/d.pl
+++ 229/d.pl
@@ -9,6 +9,36 @@ use Data::Dumper;
 chomp( my $s = <> );
 chomp( my $k = <> );
 
-my @s = split m{\s}, $s;
+my @s = split m{}, $s;
+my $n = scalar @s;
+
+push @s, ( ( q{.} ) x $k );
+
+my @dot_counts;
+my $acc = 0;
+
+for my $s ( @s ) {
+    push @dot_counts, $acc;
+    $acc++
+        if $s eq q{.};
+}
+
+push @dot_counts, $acc;
+
+my $max = 0;
+
+for ( my $i = 0; $i < $n; ++$i ) {
+    my $l = $i;
+    my $r = $#s;
+
+    while ( $l < $r ) {
+        my $c = int( ( $l + $r ) / 2 );
+        my $dots = $dot_counts[ $c + 1 ] - $dot_counts[ $i ];
+        if ( $dots 
+    }
+    my $max = 
+}
+
+say $max;
 
 exit;

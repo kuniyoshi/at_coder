f866f5b60ac778b490e1b188b9a94de476ea8c7e
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-01-05 23:34:45 +0900

    233: retrospective

--- 233/d.pl
+++ 233/d.pl
@@ -7,14 +7,39 @@ use open qw( :utf8 :std );
 use Data::Dumper;
 
 my( $n, $k ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @a = do { chomp( my $l = <> ); split m{\s}, $l };
+my @a = map { $_ + 1e9 }
+        do { chomp( my $l = <> ); split m{\s}, $l };
+$k = $k + 1e9;
 
 my $acc = 0;
-my @acc;
+my @acc = ( 0 );
 
 for ( my $i = 0; $i < @a; ++$i ) {
     $acc = $acc + $a[ $i ];
-    $acc[ $i ] = $acc;
+    push @acc, $acc;
 }
+
+push @acc, 1e17;
+
+my $total = 0;
+
+for ( my $i = 0; $i < @a; ++$i ) {
+    if ( ( $acc[ $i + 1 ] - $acc[ $i ] ) == $k ) {
+        $total++;
+        next;
+    }
+
+    my $ac = $i;
+    my $wa = $#acc;
+    my $wj = int( ( $ac + $wa ) / 2 );
+
+    if ( $acc[ $wj ] - $acc[ $ac ] 
+
+        next
+            if 
+    }
+}
+
+say $total;
 
 exit;

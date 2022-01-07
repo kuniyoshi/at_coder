b1c2b36335b9f636c74c505ebaa4c8f01e9ebdc8
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-01-07 22:41:48 +0900

    233.e: wa

--- 233/e.pl
+++ 233/e.pl
@@ -5,6 +5,30 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
+use integer;
 
+chomp( my $x = <> );
+my @bits = split m{}, $x;
+my @answers;
+
+my @acc = ( 0 );
+my $acc = 0;
+
+for my $bit ( @bits ) {
+    $acc = $acc + $bit;
+    push @acc, $acc;
+}
+
+my $rise = 0;
+
+for ( my $i = 0; $i < @bits; ++$i ) {
+    my $sum = $acc[ @bits - $i ];
+    my $total = $sum + $rise;
+    my $bit = $total % 10;
+    push @answers, $bit;
+    $rise = int( $total / 10 );
+}
+
+say join q{}, reverse @answers;
 
 exit;

a67a6708d24063bd33f5e58b877705ba87a1d9ed
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-01-25 23:54:44 +0900

    236.d: wa

--- 236/d.pl
+++ 236/d.pl
@@ -11,21 +11,55 @@ my @a = map { chomp; [ split m{\s} ] }
         map { scalar <> }
         1 .. ( 2 * $n - 1 );
 
-say dfs( [ ], [ 0 .. ( $n - 1 ) ] );
+say dfs( [ ], [ 0 .. ( 2 * $n - 1 ) ], { }, 0 );
 
 exit;
 
 sub dfs {
     my $selectins_ref = shift;
     my $buffer_ref = shift;
+    my $paired_ref = shift;
+    my $max = shift;
+
+    #    warn sprintf "### selection: [%s]", join q{, }, @{ $selectins_ref };
+    #    warn sprintf "### buffer: [%s]", join q{, }, @{ $buffer_ref };
 
     unless ( @{ $buffer_ref } ) {
-        return 
+        die "somehow"
+            unless @{ $selectins_ref } == ( 2 * $n );
+
+        say sprintf "### selection: [%s]", join q{, }, @{ $selectins_ref };
+
+        my $candidate = 0;
+
+        for ( my $i = 0; $i < $n; ++$i ) {
+            my $l = 2 * $i;
+            my $r = 2 * $i + 1;
+            die "should be ascend"
+                unless $selectins_ref->[ $l ] < $selectins_ref->[ $r ];
+            die "out of range ($l, $r)"
+                if $l >= @a || ( $r - $l - 1 ) >= @{ $a[ $l ] };
+            $candidate = $candidate ^ $a[ $l ][ $r - $l - 1 ];
+        }
+
+        return $candidate > $max ? $candidate : $max;
     }
 
+    die sprintf "buffer should be multiple 2: [%s]", join q{, }, @{ $buffer_ref }
+        if @{ $buffer_ref } % 2;
+
+    my $candidate = $max;
+
     for ( my $i = 0; $i < @{ $buffer_ref }; ++$i ) {
-        my @c = ( @{ $selectins_ref }, $buffer_ref->[ $i ] );
-        my @d = @{ $buffer_ref }[ 0 .. $i, ( $i + 1 ) .. $#{ $buffer_ref } ];
-        dfs( \@c, \@d );
+        for ( my $j = 0; $j < $i; ++$j ) {
+            #        warn "(\$i, \$j): ($i, $j)";
+            next
+                if $j == $i;# or $paired_ref->{ 10 * $j + $i }++;
+                #                if $j == $i or $paired_ref->{ 10 * $j + $i }++;
+            my @c = ( @{ $selectins_ref }, @{ $buffer_ref }[ $j, $i ] );
+            $candidate = dfs( \@c, [ @{ $buffer_ref }[ 0 .. ( $j - 1 ), ( $j + 1 ) .. ( $i - 1 ), ( $i + 1 ) .. $#{ $buffer_ref } ] ], $paired_ref, $candidate );
+        }
     }
+
+    return $candidate > $max ? $candidate : $max;
 }

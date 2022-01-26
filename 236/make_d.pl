074e64da6073476f76d2ab8893b2a177c8346ccb
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-01-26 23:07:29 +0900

    236.d: test data

---
+++ 236/make_d.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+use 5.10.0;
+use utf8;
+use strict;
+use warnings;
+use open qw( :utf8 :std );
+use Data::Dumper;
+
+my $n = 6;
+
+my $length = 2 * $n;
+
+say $n;
+
+for ( my $i = 0; $i < $length - 1; ++$i ) {
+    my $count = $length - $i - 1;
+    say join q{ }, map { int( 16 * rand ) } 1 .. $count;
+}
+
+
+exit;
+

3a15b5a127bca3c3b8e6b3e5c91e4f99318a12ff
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-02-10 23:41:07 +0900

    238.d: wip

--- 238/d.pl
+++ 238/d.pl
@@ -11,6 +11,17 @@ my @q = map { chomp; [ split m{\s} ] }
         map { scalar <> }
         1 .. $t;
 
+my %can;
+#    a  s  i  x  y
+$can{0}{0}{0}{0}{0}++;
+$can{0}{0}{1}{0}{1}++;
+$can{0}{0}{1}{1}{0}++;
+$can{0}{1}{0}{0}{1}++;
+$can{0}{1}{0}{1}{0}++;
+$can{0}{1}{1}{0}{0}++;
+$can{1}{0}{0}{1}{1}++;
+$can{1}{1}{1}{1}{1}++;
+
 for my $q_ref ( @q ) {
     my( $a, $s ) = @{ $q_ref };
     my $has = test( $a, $s, 0 );
@@ -20,12 +31,25 @@ for my $q_ref ( @q ) {
 exit;
 
 sub test {
-    my( $a, $s, $increment ) = @_;
-    return 
-    my $u = $a & 1;
-    my $v = $s & 1;
+    my( $and, $sum, $increment ) = @_;
+    #    warn sprintf "(%b, %b, %b)", $and, $sum, $increment;
+    return $can{ $and }{ $sum }{ $increment }{0}{0}
+        if $and == 0 && $sum == 0;
+    my $a = $and & 1;
+    my $s = $sum & 1;
+    my $canbe;
     for my $x ( 0 .. 1 ) {
         for my $y ( 0 .. 1 ) {
+            #            if ( $a == 1 && $s == 0 && $increment == 0 && $x == 1 && $y == 1 ) {
+            #                warn "### $a $s $increment $x $y : $can{ $a }{ $s }{ $increment }{ $x }{ $y }";
+            #            }
+            #            warn sprintf "%b, %b, %b: [%b %b %b] [%b, %b] -> %d", $and, $sum, $increment, $a, $s, $increment, $x, $y, $can{ $a }{ $s }{ $increment }{ $x }{ $y } // 0;
+            next
+                unless $can{ $a }{ $s }{ $increment }{ $x }{ $y };
+            next
+                unless test( $and >> 1, $sum >> 1, ( ( $increment + $x + $y ) & 2 ) ? 1 : 0 );
+            $canbe++;
         }
     }
+    return $canbe;
 }

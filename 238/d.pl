fedc268c4b0baedb34dfdb1c9fb293052b4377fd
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-02-11 20:12:06 +0900

    238.e: ac

--- 238/d.pl
+++ 238/d.pl
@@ -5,51 +5,43 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
+use Memoize;
+
+memoize( "test" );
 
 chomp( my $t = <> );
 my @q = map { chomp; [ split m{\s} ] }
         map { scalar <> }
         1 .. $t;
 
-my %can;
-#    a  s  i  x  y
-$can{0}{0}{0}{0}{0}++;
-$can{0}{0}{1}{0}{1}++;
-$can{0}{0}{1}{1}{0}++;
-$can{0}{1}{0}{0}{1}++;
-$can{0}{1}{0}{1}{0}++;
-$can{0}{1}{1}{0}{0}++;
-$can{1}{0}{0}{1}{1}++;
-$can{1}{1}{1}{1}{1}++;
-
 for my $q_ref ( @q ) {
     my( $a, $s ) = @{ $q_ref };
-    my $has = test( $a, $s, 0 );
+    my $has = test( $a, $s );
     say $has ? "Yes" : "No";
 }
 
 exit;
 
 sub test {
-    my( $and, $sum, $increment ) = @_;
-    #    warn sprintf "(%b, %b, %b)", $and, $sum, $increment;
-    return $can{ $and }{ $sum }{ $increment }{0}{0}
-        if $and == 0 && $sum == 0;
+    my( $and, $sum ) = @_;
+
+    return $and == 0
+        if $sum == 0;
+
     my $a = $and & 1;
-    my $s = $sum & 1;
-    my $canbe;
+
     for my $x ( 0 .. 1 ) {
         for my $y ( 0 .. 1 ) {
-            #            if ( $a == 1 && $s == 0 && $increment == 0 && $x == 1 && $y == 1 ) {
-            #                warn "### $a $s $increment $x $y : $can{ $a }{ $s }{ $increment }{ $x }{ $y }";
-            #            }
-            #            warn sprintf "%b, %b, %b: [%b %b %b] [%b, %b] -> %d", $and, $sum, $increment, $a, $s, $increment, $x, $y, $can{ $a }{ $s }{ $increment }{ $x }{ $y } // 0;
             next
-                unless $can{ $a }{ $s }{ $increment }{ $x }{ $y };
+                if $a != ( $x & $y );
             next
-                unless test( $and >> 1, $sum >> 1, ( ( $increment + $x + $y ) & 2 ) ? 1 : 0 );
-            $canbe++;
+                if ( ( $sum - $x - $y ) < 0 );
+            next
+                if ( ( ( $sum - $x - $y ) % 2 ) != 0 );
+            return 1
+                if test( $and >> 1, ( $sum - $x - $y ) >> 1 );
         }
     }
-    return $canbe;
+
+    return;
 }
--- 238/e.pl
+++ 238/e.pl
@@ -6,11 +6,61 @@ use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
 
-chomp( my $n = <> );
-my( $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @s = do { chomp( my $l = <> ); split m{\s}, $l };
-my @l = map { chomp; [ split m{\s} ] }
-        map { scalar <> }
-        1 .. $n;
+my( $n, $q ) = do { chomp( my $l = <> ); split m{\s}, $l };
+my @lr = map { chomp; [ split m{\s} ] }
+         map { scalar <> }
+         1 .. $q;
+
+my $union_find_tree = UnionFindTree->new( $n + 1 );
+
+for my $lr_ref ( @lr ) {
+    my( $l, $r ) = @{ $lr_ref };
+    $union_find_tree->unite( $r, $l - 1 );
+}
+
+my $can = $union_find_tree->root( 0 ) == $union_find_tree->root( $n );
+
+say $can ? "Yes" : "No";
+
 
 exit;
+
+package UnionFindTree;
+
+sub size {
+    my $self = shift;
+    my $u = shift;
+    return $self->{sizes}[ $self->root( $u ) ];
+}
+
+sub unite {
+    my $self = shift;
+    my( $u, $v ) = @_;
+    my( $root_u, $root_v ) = ( $self->root( $u ), $self->root( $v ) );
+
+    return
+        if $root_u == $root_v;
+
+    my( $size_u, $size_v ) = @{ $self->{sizes} }[ $root_u, $root_v ];
+    $self->{sizes}[ $root_v ] = $size_u + $size_v;
+    $self->{sizes}[ $root_u ] = $size_u + $size_v;
+    $self->{parents}[ $root_u ] = $root_v;
+}
+
+sub root {
+    my $self = shift;
+    my $v = shift;
+    my $parent = $self->{parents}[ $v ];
+
+    return $v
+        if $parent == $v;
+
+    return $self->{parents}[ $v ] = $self->root( $parent );
+}
+
+sub new {
+    my $class = shift;
+    my $n = shift;
+    my @sizes = ( 1 ) x $n;
+    return bless { parents => [ 0 .. ( $n - 1 ) ], sizes => \@sizes }, $class;
+}

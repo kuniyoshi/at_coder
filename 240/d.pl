78ae8a35ca3d49ec08593e77559a573d1aeff612
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-02-21 22:52:41 +0900

    240.d: ac

--- 240/d.pl
+++ 240/d.pl
@@ -7,10 +7,38 @@ use open qw( :utf8 :std );
 use Data::Dumper;
 
 chomp( my $n = <> );
-my( $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @s = do { chomp( my $l = <> ); split m{\s}, $l };
-my @l = map { chomp; [ split m{\s} ] }
-        map { scalar <> }
-        1 .. $n;
+my @a = do { chomp( my $l = <> ); split m{\s}, $l };
+
+my @stack;
+my $count = 0;
+
+for $a ( @a ) {
+    if ( !@stack ) {
+        push @stack, [ $a, 1 ];
+        $count++;
+        say $count;
+        next;
+    }
+
+    if ( $stack[-1][0] != $a ) {
+        push @stack, [ $a, 1 ];
+        $count++;
+        say $count;
+        next;
+    }
+
+    $stack[-1][1]++;
+    $count++;
+
+    while ( @stack && $stack[-1][1] >= $stack[-1][0] ) {
+        $stack[-1][1] = $stack[-1][1] - $stack[-1][0];
+        $count = $count - $stack[-1][0];
+        pop @stack
+            if $stack[-1][1] == 0;
+    }
+
+    say $count;
+}
+
 
 exit;

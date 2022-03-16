bd87f481e573e9af64e119893a76d449565eb0ba
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-03-16 23:16:27 +0900

    242: retrospective after explanation

--- 242/c2.pl
+++ 242/c2.pl
@@ -5,7 +5,6 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
-use List::Util qw( sum );
 
 my $MOD = 998244353;
 
@@ -18,17 +17,23 @@ $dp[0][ $_ ] = 1
 for my $i ( 1 .. ( $n - 1 ) ) {
     for my $j ( 1 .. 9 ) {
         if ( $j == 1 ) {
-            $dp[ $i ][ $j - 1 ] = sum( @{ $dp[ $i - 1 ] }[0, 1] ) % $MOD;
+            $dp[ $i ][ $j - 1 ] = ( $dp[ $i - 1 ][0] + $dp[ $i - 1 ][1] ) % $MOD;
         }
         elsif ( $j == 9 ) {
-            $dp[ $i ][ $j - 1 ] = sum( @{ $dp[ $i - 1 ] }[7, 8] ) % $MOD;
+            $dp[ $i ][ $j - 1 ] = ( $dp[ $i - 1 ][7] + $dp[ $i - 1 ][8] ) % $MOD;
         }
         else {
-            $dp[ $i ][ $j - 1 ] = sum( @{ $dp[ $i - 1 ] }[ $j - 2, $j - 1, $j ] ) % $MOD;
+            $dp[ $i ][ $j - 1 ] = ( $dp[ $i - 1 ][ $j - 2 ] + $dp[ $i - 1 ][ $j - 1 ] + $dp[ $i - 1 ][ $j ] ) % $MOD;
         }
     }
 }
 
-say sum( @{ $dp[ $n - 1 ] } ) % $MOD;
+my $answer = 0;
+
+for my $i ( 0 .. 8 ) {
+    $answer = ( $answer + $dp[ $n - 1 ][ $i ] ) % $MOD;
+}
+
+say $answer;
 
 exit;

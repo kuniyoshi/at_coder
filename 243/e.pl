6e433801dd20a126d192317f94d1823ed6fedef2
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-03-19 16:47:38 +0900

    243.e: wip, retrospective

--- 243/e.pl
+++ 243/e.pl
@@ -7,67 +7,32 @@ use open qw( :utf8 :std );
 use Data::Dumper;
 
 my( $n, $m ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @l = sort { $a->[-1] <=> $b->[-1] }
-        map { chomp; [ split m{\s} ] }
+my @l = map { chomp; [ split m{\s} ] }
         map { scalar <> }
         1 .. $m;
 
-my $tree = UnionFindTree->new( $n );
+my @dp;
 
-my $skips = 0;
+for my $i ( 0 .. ( $n - 1 ) ) {
+    for my $j ( 0 .. ( $n - 1 ) ) {
+        $dp[ $i ][ $j ] = 1e10;
+    }
+}
 
 for my $edge_ref ( @l ) {
-    my( $a, $b, $c ) = @{ $edge_ref };
-    $a--;
-    $b--;
-    if ( $tree->root( $a ) == $tree->root( $b ) ) {
-        $skips++;
-        next;
-    }
-    $tree->unite( $a, $b );
+    my( $from, $to, $cost ) = @{ $edge_ref };
+    $dp[ $from ][ $to ] = $cost;
+    $dp[ $to ][ $from ] = $cost;
 }
 
-warn Dumper $tree;
-say $skips;
+for my $k ( 0 .. ( $n - 1 ) ) {
+    for my $i ( 0 .. ( $n - 1 ) ) {
+        for my $j ( 0 .. ( $n - 1 ) ) {
+            my $candidate = $dp[ $i ][ $k ] + $dp[ $k ][ $j ];
+            $dp[ $i ][ $j ] = $candidate
+                if $candidate < $dp[ $i ][ $j ];
+        }
+    }
+}
 
 exit;
-
-package UnionFindTree;
-
-sub size {
-    my $self = shift;
-    my $u = shift;
-    return $self->{sizes}[ $self->root( $u ) ];
-}
-
-sub unite {
-    my $self = shift;
-    my( $u, $v ) = @_;
-    my( $root_u, $root_v ) = ( $self->root( $u ), $self->root( $v ) );
-
-    return
-        if $root_u == $root_v;
-
-    my( $size_u, $size_v ) = @{ $self->{sizes} }[ $root_u, $root_v ];
-    $self->{sizes}[ $root_v ] = $size_u + $size_v;
-    $self->{sizes}[ $root_u ] = $size_u + $size_v;
-    $self->{parents}[ $root_u ] = $root_v;
-}
-
-sub root {
-    my $self = shift;
-    my $v = shift;
-    my $parent = $self->{parents}[ $v ];
-
-    return $v
-        if $parent == $v;
-
-    return $self->{parents}[ $v ] = $self->root( $parent );
-}
-
-sub new {
-    my $class = shift;
-    my $n = shift;
-    my @sizes = ( 1 ) x $n;
-    return bless { parents => [ 0 .. ( $n - 1 ) ], sizes => \@sizes }, $class;
-}
---
+++ 243/generate_e.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+use 5.10.0;
+use utf8;
+use strict;
+use warnings;
+use open qw( :utf8 :std );
+use Data::Dumper;
+
+my $n = 300;
+
+my @linked;
+
+my $count = $n / 2 * ( $n - 1 );
+
+say "$n $count";
+
+for my $i ( 0 .. $n - 1 ) {
+    for my $j ( 0 .. $i - 1 ) {
+        next
+            if $linked[ $i ][ $j ]++;
+        next
+            if $linked[ $j ][ $i ]++;
+        next
+            if $j == $i;
+
+        say "$i $j 1";
+    }
+}
+
+
+exit;
+

60a290301d56fd4e4bd5181c8b87ce2ef0815c68
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-03-26 15:41:21 +0900

    244.e: wa,tle

--- 244/e.pl
+++ 244/e.pl
@@ -6,11 +6,61 @@ use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
 
-chomp( my $n = <> );
-my( $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @s = do { chomp( my $l = <> ); split m{\s}, $l };
-my @l = map { chomp; [ split m{\s} ] }
-        map { scalar <> }
-        1 .. $n;
+my $MOD = 998244353;
+
+my( $n, $m, $k, $s, $t, $x ) = do { chomp( my $l = <> ); split m{\s}, $l };
+my @links = map { chomp; [ split m{\s} ] }
+            map { scalar <> }
+            1 .. $m;
+$s--;
+$t--;
+$x--;
+
+my %edges;
+
+for my $link_ref ( @links ) {
+    my( $from, $to ) = @{ $link_ref };
+    $from--;
+    $to--;
+    push @{ $edges{ $from } }, $to;
+    push @{ $edges{ $to } }, $from;
+}
+
+my @dp;
+$dp[0][ $_ ][0] = $dp[0][ $_ ][1] = 0
+    for 0 .. $n - 1;
+
+$dp[0][ $s ][0] = 1; # s != x
+
+for my $ki ( 1 .. $k ) {
+    for my $v ( 0 .. $n - 1 ) {
+        next
+            unless $edges{ $v };
+
+        if ( $v == $x ) {
+            for my $eo ( 0 .. 1 ) {
+                my $invert = $eo ^ 1;
+
+                for my $neighbor ( @{ $edges{ $v } } ) {
+                    $dp[ $ki ][ $v ][ $eo ] += $dp[ $ki - 1 ][ $neighbor ][ $invert ];
+                    $dp[ $ki ][ $v ][ $eo ] %= $MOD;
+                }
+            }
+        }
+        else {
+            for my $eo ( 0 .. 1 ) {
+                for my $neighbor ( @{ $edges{ $v } } ) {
+                    $dp[ $ki ][ $v ][ $eo ] += $dp[ $ki - 1 ][ $neighbor ][ $eo ];
+                    $dp[ $ki ][ $v ][ $eo ] %= $MOD;
+                }
+            }
+        }
+
+    }
+}
+
+
+say $dp[ $k ][ $t ][0];
+
 
 exit;

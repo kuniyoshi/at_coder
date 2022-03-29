a8e96c78014d2231a7471c11ae72a615cf7f317e
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-03-29 22:57:14 +0900

    245.d: ac

--- 245/d.pl
+++ 245/d.pl
@@ -11,12 +11,51 @@ my @a = do { chomp( my $l = <> ); split m{\s}, $l };
 my @c = do { chomp( my $l = <> ); split m{\s}, $l };
 
 my @b;
-$b[ $m ] = $c[ $n + $m ] / $a[ $n ];
+my $remain = $m;
 
-for my $i ( reverse( 0 .. $n + $m - 1 ) ) {
-    $c[ $i ] = 
-    $b[ $i ] = ( $c[ $n + $i ] + $a[ $n + $m - $i - 1 ] * $b[ $i + 1 ] ) / $a[ $n + $m - $i ];
+while ( $remain > 0 ) {
+    LOOP_C_INDEX:
+    for my $i ( reverse( 0 .. $n + $m ) ) { # c
+        my $indexes_ref;
+        my $acc = 0;
+
+        my $max_j = $i < $n ? $i : $n;
+
+        LOOP_A_INDEX:
+        for my $j ( reverse( 0 .. $max_j ) ) { # a
+
+            my $k = $i - $j;
+
+            next
+                if $k > $m;
+
+            next
+                if $a[ $j ] == 0 && !defined $b[ $k ];
+
+            if ( !defined $b[ $k ] ) {
+                $indexes_ref = [ $j, $k ];
+                next;
+            }
+
+            if ( !defined $b[ $k ] ) {
+                next LOOP_C_INDEX;
+            }
+
+            $acc = $acc + $a[ $j ] * $b[ $k ];
+        }
+
+        if ( !$indexes_ref ) {
+            next LOOP_C_INDEX;
+        }
+
+        my( $a_index, $b_index ) = @{ $indexes_ref };
+        $b[ $b_index ] = ( $c[ $i ] - $acc ) / $a[ $a_index ];
+        $remain--;
+    }
 }
+
+die "Could not complete b", Dumper \@b
+    if $#b != $m || grep { !defined } @b;
 
 say join q{ }, @b;
 

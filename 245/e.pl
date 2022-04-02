ede5c88866e0e4179326514cc9af6ff6bb56ad17
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-04-02 20:47:23 +0900

    245.e: wip

--- 245/e.pl
+++ 245/e.pl
@@ -9,7 +9,8 @@ use Data::Dumper;
 chomp( my $n = <> );
 my( $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
 my @s = do { chomp( my $l = <> ); split m{\s}, $l };
-my @l = map { chomp; [ split m{\s} ] }
+my @l = sort { $b->[1] <=> $a->[1] }
+        map { chomp; [ split m{\s} ] }
         map { scalar <> }
         1 .. $n;
 

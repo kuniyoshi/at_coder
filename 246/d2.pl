9fa1440764b1bb99ac140cb2fdd0050973adf976
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-04-09 20:59:55 +0900

    246.d: wip

--- 246/d2.pl
+++ 246/d2.pl
@@ -5,6 +5,7 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
+use POSIX ( );
 
 chomp( my $n = <> );
 
@@ -14,16 +15,18 @@ if ( $n == 0 ) {
 }
 
 my @ac = ( 1e6, 1e6 );
-my @wa = ( 0, 0 );
+my @wa = ( -1, -1 );
 
-my @previous = ( -1, -1 );
+my @diff = ( $ac[0] - $wa[0], $ac[1] - $wa[1] );
 
-while ( $ac[0] - $wa[0] + $ac[1] - $wa[1] > 0 ) {
+#while ( $ac[0] - $wa[0] + $ac[1] - $wa[1] > 1 ) {
+my $d = 8;
+while ( 1 ) {
     warn "($wa[0], $wa[1]), ($ac[0], $ac[1])";
     my $l0 = $wa[0];
     my $r0 = $ac[0];
 
-    while ( ( my $wj = int( ( $r0 + $l0 ) / 2 ) ) > $l0 ) {
+    while ( ( my $wj = POSIX::floor( ( $r0 + $l0 ) / 2 ) ) > $l0 ) {
         warn "($l0, $wj, $r0)";
         if ( f( $wj, $ac[1] ) >= $n ) {
             $r0 = $wj;
@@ -36,7 +39,8 @@ while ( $ac[0] - $wa[0] + $ac[1] - $wa[1] > 0 ) {
     my $l1 = $wa[1];
     my $r1 = $ac[1];
 
-    while ( ( my $wj = int( ( $r1 + $l1 ) / 2 ) ) > $l1 ) {
+    while ( ( my $wj = POSIX::floor( ( $r1 + $l1 ) / 2 ) ) > $l1 ) {
+        warn "($l1, $wj, $r1)";
         if ( f( $r0, $wj ) >= $n ) {
             $r1 = $wj;
         }
@@ -50,12 +54,16 @@ while ( $ac[0] - $wa[0] + $ac[1] - $wa[1] > 0 ) {
     $wa[0] = $l0;
     $wa[1] = $l1;
 
-    if ( $ac[0] == $previous[0] && $ac[1] == $previous[1] ) {
-        last;
+    my @new = ( $ac[0] - $wa[0], $ac[1] - $wa[1] );
+
+    if ( $new[0] == $diff[0] && $new[1] == $diff[1] ) {
+        $d--;
+        last
+            unless $d;
     }
 
-    $previous[0] = $ac[0];
-    $previous[1] = $ac[1];
+    $diff[0] = $new[0];
+    $diff[1] = $new[1];
 }
 
 say f( $ac[0], $ac[1] );

51aea568adeaae502d170b7d1923c97b393538f1
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-04-28 23:46:59 +0900

    249.d: wip

--- 249/d.pl
+++ 249/d.pl
@@ -7,10 +7,24 @@ use open qw( :utf8 :std );
 use Data::Dumper;
 
 chomp( my $n = <> );
-my( $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @s = do { chomp( my $l = <> ); split m{\s}, $l };
-my @l = map { chomp; [ split m{\s} ] }
-        map { scalar <> }
-        1 .. $n;
+my @a = do { chomp( my $l = <> ); split m{\s}, $l };
+@a = sort { $b <=> $a } @a;
+
+my %exists;
+
+my $count = 0;
+
+for my $i ( 0 .. $#a ) {
+    for my $j ( $i .. $#a ) {
+        my $x = $a[ $i ] * $a[ $j ];
+        $count += 2 * ( $exists{ $x } // 0 );
+        $count += 2 * ( $exists{ $x } // 0 )
+            if $a[ $j ] == 1;
+    }
+
+    $exists{ $a[ $i ] }++;
+}
+
+say $count;
 
 exit;

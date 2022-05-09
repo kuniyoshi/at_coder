2ce5246d732e5c0f50266680196ad9ba511abd9e
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-05-09 23:29:41 +0900

    250.d: ac

--- 250/d.pl
+++ 250/d.pl
@@ -6,12 +6,45 @@ use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
 
+my $MAX = 1e6;
+my $SUB_MAX = 1e3;
+
 chomp( my $n = <> );
 
+my @primes = primes( );
 
+my $count = 0;
+
+for my $i ( 0 .. $#primes ) {
+    last
+        if 2 * $primes[ $i ] * $primes[ $i ] * $primes[ $i ] > $n;
+
+    for my $j ( 0 .. $i - 1 ) {
+        last
+            if $primes[ $j ] * $primes[ $i ] * $primes[ $i ] * $primes[ $i ] > $n;
+        $count++;
+    }
+}
+
+say $count;
 
 exit;
 
-sub furui {
-    my 
+sub primes {
+    my @results;
+
+    my @furui = ( 1 ) x $MAX;
+    $furui[0] = 0;
+    $furui[1] = 0;
+
+    for my $i ( 0 .. $SUB_MAX + 1 ) {
+        next
+            unless $furui[ $i ];
+
+        for ( my $j = $i * $i; $j < $MAX; $j += $i ) {
+            $furui[ $j ] = 0;
+        }
+    }
+
+    return grep { $furui[ $_ ] } 0 .. $#furui;
 }

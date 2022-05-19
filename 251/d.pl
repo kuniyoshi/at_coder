f7db90cd353ff97f491d52652417bd6480002a20
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-05-19 23:48:19 +0900

    251.d: ac

--- 251/d.pl
+++ 251/d.pl
@@ -5,32 +5,16 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
-use List::Util qw( max );
 
 chomp( my $w = <> );
 
-my @results;
+my @numbers = (
+    ( 1 .. 99 ),
+    ( map { 100 * $_ } 1 .. 99 ),
+    ( map { 10000 * $_ } 1 .. 99 ),
+);
 
-while ( $w ) {
-    my %count;
-
-    for my $i ( 1 .. $w ) {
-        my $j = $w - $i;
-        last
-            if $i >= $j;
-        $count{ $i }++;
-        $count{ $j }++;
-    }
-
-    last
-        unless %count;
-
-    my( $max_ref ) = sort { $b->[1] <=> $a->[1] } map { [ $_, $count{ $_ } ] } keys %count;
-    push @results, $max_ref->[0];
-    $w -= $max_ref->[0];
-}
-
-say scalar @results;
-say join q{ }, @results;
+say scalar @numbers;
+say join q{ }, @numbers;
 
 exit;

2be027e8d043fa4baa1fdcdca4e2be24cf242e54
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-05-26 22:36:09 +0900

    252.d: ac

--- 252/d.pl
+++ 252/d.pl
@@ -10,38 +10,42 @@ chomp( my $n = <> );
 my @a = do { chomp( my $l = <> ); split m{\s}, $l };
 
 my %count;
+$count{ $_ }++
+    for @a;
+
 my $duplicated = 0;
 
-for my $a ( @a ) {
-    $duplicated++
-        if $count{ $a }++;
+for my $a ( keys %count ) {
+    $duplicated += $count{ $a } * ( $count{ $a } - 1 ) / 2;
 }
 
-warn "\$duplicated: $duplicated";
-warn Dumper \%count;
+#warn "\$duplicated: $duplicated";
+#warn Dumper \%count;
 
 my $total = 0;
 
 for my $i ( 0 .. $#a - 2 ) {
-    warn "### \$i: $i";
+    #warn "### \$i: $i";
     die "invalid count of $a[ $i ]"
         unless $count{ $a[ $i ] };
 
     my $combination_count = combination2( scalar( @a ) - ( $i + 1 ) - ( $count{ $a[ $i ] } - 1 ) );
-    warn "--- \$combination_count: $combination_count";
-    warn "--- before \$total: $total";
-    my $delta = $combination_count - ( $duplicated - ( $count{ $a[ $i ] } - 1 ) );
-    warn "--- \$delta: $delta";
+    #warn "--- \$combination_count: $combination_count";
+    #warn "--- before \$total: $total";
+    my $delta = $combination_count - ( $duplicated - ( $count{ $a[ $i ] } * ( $count{ $a[ $i ] } - 1 ) / 2 ) );
+    #warn "--- \$delta: $delta";
 
     if ( $delta > 0 ) {
         $total += $delta;
-        warn "--- after \$total: $total";
+        #warn "--- after \$total: $total";
     }
 
-    if ( $count{ $a[ $i ] } > 1 ) {
-        $duplicated--;
+    if ( $count{ $a[ $i ] } ) {
+        my $before = $count{ $a[ $i ] } * ( $count{ $a[ $i ] } - 1 ) / 2;
         $count{ $a[ $i ] }--;
-        warn "--- after \$duplicated: $duplicated";
+        my $after = $count{ $a[ $i ] } * ( $count{ $a[ $i ] } - 1 ) / 2;
+        $duplicated -= $before - $after;
+        #warn "--- after \$duplicated: $duplicated";
     }
 }
 

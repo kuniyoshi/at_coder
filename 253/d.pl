2c82b98a95a3196e005bd10a76ed9ea1e5bb0cbd
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-06-03 22:50:01 +0900

    253.d: ac

--- 253/d.pl
+++ 253/d.pl
@@ -11,7 +11,7 @@ my( $n, $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
 my $whole = ( 1 + $n ) * $n / 2;
 my $sum_a = calc( $n, $a );
 my $sum_b = calc( $n, $b );
-my $sum_ab = calc( $n, $a * $b );
+my $sum_ab = calc( $n, lcm( $a, $b ) );
 
 say $whole - $sum_a - $sum_b + $sum_ab;
 
@@ -22,4 +22,16 @@ sub calc {
     my $count = int( $n / $a );
     my $sum = $count * ( $a + $count * $a ) / 2;
     return $sum;
+}
+
+sub gcd {
+    my( $a, $b ) = @_;
+    return $a
+        if $b == 0;
+    return gcd( $b, $a % $b );
+}
+
+sub lcm {
+    my( $a, $b ) = @_;
+    return $a / gcd( $a, $b ) * $b;
 }

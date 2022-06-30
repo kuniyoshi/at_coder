1991b4a38c99bc3739baaa2323217c35a29c5fd1
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-06-30 23:23:40 +0900

    256.d: ac

--- 256/d.pl
+++ 256/d.pl
@@ -5,12 +5,32 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
+use List::Util qw( max );
 
 chomp( my $n = <> );
-my( $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @s = do { chomp( my $l = <> ); split m{\s}, $l };
-my @l = map { chomp; [ split m{\s} ] }
-        map { scalar <> }
-        1 .. $n;
+my @spans = sort { $a->[0] <=> $b->[0] }
+            map { chomp; [ split m{\s} ] }
+            map { scalar <> }
+            1 .. $n;
+
+my @finals;
+
+my $left = shift @spans;
+
+while ( @spans ) {
+    my $right = shift @spans;
+
+    if ( $right->[0] > $left->[1] ) {
+        push @finals, $left;
+        $left = $right;
+        next;
+    }
+
+    $left->[1] = max( $left->[1], $right->[1] );
+}
+
+push @finals, $left;
+
+print map { "$_->[0] $_->[1]\n" } @finals;
 
 exit;

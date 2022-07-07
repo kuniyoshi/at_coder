6f9ac2bfbee083a2690c1aca319bef8eb7d16935
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-07-07 23:34:49 +0900

    257.e: ac

--- 257/e.pl
+++ 257/e.pl
@@ -5,12 +5,31 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
+use List::Util qw( min );
 
 chomp( my $n = <> );
-my( $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @s = do { chomp( my $l = <> ); split m{\s}, $l };
-my @l = map { chomp; [ split m{\s} ] }
-        map { scalar <> }
-        1 .. $n;
+my @c = do { chomp( my $l = <> ); split m{\s}, $l };
+
+my @numbers;
+
+my $min = min( @c );
+my $k = int( $n / $min );
+my $remain = $n;
+
+while ( $k ) {
+    for my $i ( reverse 1 .. 9 ) {
+        next
+            if $remain < $c[ $i - 1 ];
+        my $new_k = int( ( $remain - $c[ $i - 1 ] ) / $min );
+        next
+            if $new_k != $k - 1;
+        push @numbers, $i;
+        $remain -= $c[ $i - 1 ];
+        last;
+    }
+    $k--;
+}
+
+say join q{}, @numbers;
 
 exit;

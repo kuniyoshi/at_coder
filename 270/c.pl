7e91c91a42bc95cd5096ac2f38fb584eef9c3a8a
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-09-25 12:57:25 +0900

    270.c: tle

--- 270/c.pl
+++ 270/c.pl
@@ -14,185 +14,134 @@ my @edges = map { chomp; [ split m{\s} ] }
 $x--;
 $y--;
 
-my %link;
+my %links_of;
 
 for my $ref ( @edges ) {
     my( $u, $v ) = @{ $ref };
     $u--;
     $v--;
-    $link{ $u }{ $v }++;
-    $link{ $v }{ $u }++;
+    push @{ $links_of{ $u } }, $v;
+    push @{ $links_of{ $v } }, $u;
 }
 
 my @d;
 
 for my $i ( 0 .. $n - 1 ) {
-    for my $j ( 0 .. $n - 1 ) {
-        $d[ $i ][ $j ] = $link{ $i }{ $j } ? 1 : 2 * $n;
-    }
+    $d[ $i ] = $i == $x ? 0 : 2 * $n;
 }
 
-#my @passes;
+my %previous;
 
-for my $k ( 0 .. $n - 1 ) {
-    for my $i ( 0 .. $n - 1 ) {
-        for my $j ( 0 .. $n - 1 ) {
-            if ( $d[ $i ][ $j ] > $d[ $i ][ $k ] + $d[ $k ][ $j ] ) {
-                $d[ $i ][ $j ] = $d[ $i ][ $k ] + $d[ $k ][ $j ];
-                #                push @{ $passes[ $i ][ $j ] }, $k;
-            }
+my $queue = PriorityQueue::PriorSmall->new;
+$queue->push( [ $d[ $x ], $x ] );
+
+while ( $queue->size ) {
+    my $ref = $queue->pop;
+    my $u = $ref->[1];
+
+    for my $v ( @{ $links_of{ $u } } ) {
+        my $alternative = $d[ $u ] + 1;
+
+        if ( $alternative < $d[ $v ] ) {
+            $d[ $v ] = $alternative;
+            $previous{ $v } = $u;
+            $queue->push( [ $d[ $v ], $v ] );
         }
     }
 }
 
-#say join q{ }, map { $_ + 1 } ( $x, @{ $passes[ $x ][ $y ] }, $y );
+my $cursor = $y;
+my @passes;
 
-say join q{ }, map { $_ + 1 } djkstra( );
+while ( defined $previous{ $cursor } ) {
+    push @passes, $cursor;
+    $cursor = $previous{ $cursor };
+}
+
+push @passes, $x;
+
+say join q{ }, map { $_ + 1 } reverse @passes;
 
 exit;
 
-sub djkstra {
-    my @queue = ( $x );
-    my @passes = ( $x );
-    my %visited;
+use strict;
+use warnings;
 
-    while ( @queue ) {
-        my $v = shift @queue;
-        next
-            if $visited{ $v }++;
+package PriorityQueue::PriorSmall;
 
-        for my $u ( grep { !$visited{ $_ } } keys %{ $link{ $v } } ) {
-            if ( $u == $y ) {
-                return @passes, $u;
-            }
-
-            #            warn "($v -> $u) if $d[ $u ][ $y ] < $d[ $v ][ $y ]";
-            if ( $d[ $u ][ $y ] < $d[ $v ][ $y ] ) {
-                push @queue, $u;
-                push @passes, $u;
-            }
-        }
-    }
-
-    die "Could not found passes";
+sub new {
+    my $class = shift;
+    return bless { items => [ ] }, $class;
 }
 
-
-
-__END__
-
-my @passes = bfs( );
-say join q{ }, map { $_ + 1 } @passes;
-
-exit;
-
-sub bfs {
-    my @queue = map { [ $_, [ $x ] ] }
-                @{ $link{ $x } };
-    my %visited = ( $x => 1 );
-
-    if ( grep { $_ == $y } @{ $link{ $x } } ) {
-        return $x, $y;
-    }
-
-    while ( @queue ) {
-        my $ref = shift @queue;
-        my $v = $ref->[0];
-        next
-            if $visited{ $v }++;
-
-        if ( grep { $_ == $y } @{ $link{ $v } } ) {
-            return @{ $ref->[1] }, $v, $y;
-        }
-
-        for my $u ( @{ $link{ $v } } ) {
-            next
-                if $visited{ $u };
-            push @queue, [ $u, [ @{ $ref->[1] }, $v ] ];
-        }
-    }
-
-    die "Could not find passes";
-}
-__END__
-
-#for ( my $k = 0; $k < $n - 1; ++$k ) {
-#    for ( my $i = 0; $i < $n - 1; ++$i ) {
-#        for ( my $j = 0; $j < $n - 1; ++$j ) {
-#            if ( $d[ $i ][ $j ] > $d[ $i ][ $k ] + $d[ $k ][ $j ] ) {
-#                $d[ $i ][ $j ] = $d[ $i ][ $k ] + $d[ $k ][ $j ];
-#            }
-#        }
-#    }
-#}
-
-die Dumper \@d;
-
-my @passes = djkstra( );
-
-exit;
-
-sub djkstra {
-    my @queue = ( $x );
-    my @passes = ( $x );
-    my %visited;
-
-    while ( @queue ) {
-        my $v = shift @queue;
-        next
-            if $visited{ $v }++;
-
-        if ( $v == $y ) {
-            return @passes;
-        }
-
-        for my $u ( grep { !$visited{ $_ } } keys %{ $link{ $v } } ) {
-            warn "($v -> $u)";
-            warn "$d[ $u ][ $y ], $d[ $v ][ $y ]";
-            if ( $d[ $u ][ $y ] < $d[ $v ][ $y ] ) {
-                push @queue, $u;
-                push @passes, $u;
-            }
-        }
-    }
-
-    die "Could not found passes";
+sub dump {
+    my $self = shift;
+    return join q{, }, @{ $self->{items} };
 }
 
-
-
-__END__
-
-my @passes = bfs( );
-say join q{ }, map { $_ + 1 } @passes;
-
-exit;
-
-sub bfs {
-    my @queue = map { [ $_, [ $x ] ] }
-                @{ $link{ $x } };
-    my %visited = ( $x => 1 );
-
-    if ( grep { $_ == $y } @{ $link{ $x } } ) {
-        return $x, $y;
-    }
-
-    while ( @queue ) {
-        my $ref = shift @queue;
-        my $v = $ref->[0];
-        next
-            if $visited{ $v }++;
-
-        if ( grep { $_ == $y } @{ $link{ $v } } ) {
-            return @{ $ref->[1] }, $v, $y;
-        }
-
-        for my $u ( @{ $link{ $v } } ) {
-            next
-                if $visited{ $u };
-            push @queue, [ $u, [ @{ $ref->[1] }, $v ] ];
-        }
-    }
-
-    die "Could not find passes";
+sub push {
+    my $self = shift;
+    my $value = shift;
+    Heap::reverse_push_to( $self->{items}, $value );
 }
+
+sub pop {
+    my $self = shift;
+
+    die "Could not pop while buffer is empty"
+        unless @{ $self->{items} };
+
+    return Heap::reverse_pop_from( $self->{items} );
+}
+
+sub peek {
+    return shift->{items}[0];
+}
+
+sub size {
+    return scalar @{ shift->{items} };
+}
+
+package Heap;
+
+sub reverse_push_to {
+    my( $buffer_ref, $item ) = @_;
+
+    push @{ $buffer_ref }, $item;
+
+    my $cursor = $#{ $buffer_ref };
+
+    while ( $cursor != 0 ) {
+        my $parent = int( ( $cursor - 1 ) / 2 );
+        @{ $buffer_ref }[ $parent, $cursor ] = @{ $buffer_ref }[ $cursor, $parent ]
+            if $buffer_ref->[ $parent ][0] >= $buffer_ref->[ $cursor ][0];
+        $cursor = $parent;
+    }
+}
+
+sub reverse_pop_from {
+    my $buffer_ref = shift;
+
+    my $last_root = $buffer_ref->[0];
+    $buffer_ref->[0] = $buffer_ref->[-1];
+    $#{ $buffer_ref }--;
+
+    my $cursor = 0;
+
+    while ( ( my $left = 2 * $cursor + 1 ) < @{ $buffer_ref } ) {
+        my $right = $left + 1;
+
+        my $child = $right < @{ $buffer_ref } && $buffer_ref->[ $left ][0] >= $buffer_ref->[ $right ][0]
+            ? $right
+            : $left;
+
+        @{ $buffer_ref }[ $cursor, $child ] = @{ $buffer_ref }[ $child, $cursor ]
+            if $buffer_ref->[ $cursor ][0] >= $buffer_ref->[ $child ][0];
+
+        $cursor = $child;
+    }
+
+    return $last_root;
+}
+
+1;

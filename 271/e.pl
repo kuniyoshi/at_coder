69644c3109758ee7ced551cb1c4862e53a60981d
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-10-10 12:31:21 +0900

    271.e: ac

--- 271/e.pl
+++ 271/e.pl
@@ -5,56 +5,33 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
-use List::Util qw( min );
+use List::Util ( );
 
 my( $n, $m, $k ) = do { chomp( my $l = <> ); split m{\s}, $l };
 my @edges = map { chomp; [ split m{\s} ] }
             map { scalar <> }
             1 .. $m;
 my @paths = do { chomp( my $l = <> ); split m{\s}, $l };
-my %connection;
 
-for my $ref ( @edges ) {
-    $connection{ $ref->[0] }{ $ref->[1] }++;
-    $connection{ $ref->[1] }{ $ref->[0] }++;
+my @costs = ( 0 );
+
+for my $path ( @paths ) {
+    my( $u, $v, $cost ) = @{ $edges[ $path - 1 ] };
+    $u--;
+    $v--;
+    $costs[ $v ] = !defined $costs[ $u ] ? $costs[ $v ] : min2( $costs[ $u ] + $cost, $costs[ $v ] );
 }
 
-my $cost = 0;
-my $min = undef;
-my $current = 1;
-my %passes = ( 1 => 1 );
-
-for my $i ( 0 .. $#paths ) {
-    my( $u, $v, $c ) = @{ $edges[ $paths[ $i ] - 1 ] };
-
-    if ( $u != $current ) {
-        $current = 1;
-        $cost = 0;
-        %passes = ( );
-
-        if ( $u == 1 ) {
-            $cost = $c;
-            %passes = ( $u => 1, $v => 1 );
-            $current = $v;
-        }
-
-        next;
-    }
-
-    if ( $v == $n ) {
-        $min = min( $min // $cost + $c, $cost + $c );
-        $cost = 0;
-        %passes = ( );
-        $current = 1;
-        next;
-    }
-
-    $current = $v;
-    $cost += $c;
-    $passes{ $u }++;
-    $passes{ $v }++;
-}
-
-say $min // -1;
+say $costs[ $n - 1 ] // -1;
 
 exit;
+
+sub min2 {
+    my $new = shift;
+    my $current = shift;
+    die "new should be defined"
+        unless defined $new;
+    return $new
+        if !defined $current;
+    return List::Util::min( $new, $current );
+}

9e975ba8e7cad186e4a26e2adf8e11471b14d99b
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-11-06 13:57:22 +0900

    273.c: ac

--- 273/c.pl
+++ 273/c.pl
@@ -7,10 +7,28 @@ use open qw( :utf8 :std );
 use Data::Dumper;
 
 chomp( my $n = <> );
-my( $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @s = do { chomp( my $l = <> ); split m{\s}, $l };
-my @l = map { chomp; [ split m{\s} ] }
-        map { scalar <> }
-        1 .. $n;
+my @numbers = do { chomp( my $l = <> ); split m{\s}, $l };
+
+my @values;
+
+for my $number ( sort { $a <=> $b } @numbers ) {
+    if ( !@values || $values[-1][0] != $number ) {
+        push @values, [ $number, 1 ];
+    }
+    else {
+        $values[-1][1]++;
+    }
+}
+
+my %count;
+
+for my $i ( 0 .. $#values ) {
+    my $kinds = $#values - $i;
+    $count{ $kinds } = $values[ $i ][1];
+}
+
+for my $i ( 0 .. $n - 1 ) {
+    say $count{ $i } // 0;
+}
 
 exit;

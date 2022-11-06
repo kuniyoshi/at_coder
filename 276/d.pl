ec757d742a84282fee6b656e221fb046da694528
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-11-06 10:21:27 +0900

    276.d: clean up code

--- 276/d.pl
+++ 276/d.pl
@@ -1,11 +1,11 @@
-#!/usr/bin/env perl
+#!/opt/local/bin/perl5.26
 use 5.10.0;
 use utf8;
 use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
-use List::Util qw( min );
+use List::Util qw( min all sum );
 
 chomp( my $n = <> );
 my @a = do { chomp( my $l = <> ); split m{\s}, $l };
@@ -17,36 +17,32 @@ say $count;
 exit;
 
 sub answer {
-    my $total = 0;
-    my $base;
-
-    my @counts;
+    my %counts;
+    my @bases;
 
     for my $i ( 0 .. $#a ) {
         my $value = $a[ $i ];
 
         for my $dividor ( 2, 3 ) {
-            $counts[ $i ]{ $dividor } = 0;
+            $counts{ $dividor }[ $i ] = 0;
 
             while ( ( $value % $dividor ) == 0 ) {
                 $value /= $dividor;
-                $total++;
-                $counts[ $i ]{ $dividor }++;
+                $counts{ $dividor }[ $i ]++;
             }
         }
 
-        if ( !defined $base ) {
-            $base = $value;
-        }
-        elsif ( $value != $base ) {
-            return;
-        }
+        push @bases, $value;
     }
+
+    return
+        unless all { $_ == $bases[0] } @bases;
 
     for my $dividor ( 2, 3 ) {
-        my $min = min( map { $_->{ $dividor } } @counts );
-        $total -= $min * @a;
+        my $min = min( @{ $counts{ $dividor } } );
+        $_ -= $min
+            for @{ $counts{ $dividor } };
     }
 
-    return $total;
+    return sum( map { @{ $_ } } values %counts );
 }

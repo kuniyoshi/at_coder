505fe4e689bcf3f09190ffcc7e7419175152fffb
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-11-19 16:57:03 +0900

    275.c: ac

--- 275/c.pl
+++ 275/c.pl
@@ -16,47 +16,19 @@ my @points = map {
     grep { $cells[ $_->[0] ][ $_->[1] ] eq q{#} } map { [ $r, $_ ] } 0 .. 8;
 } 0 .. 8;
 
-my %distance;
-
-for my $i ( 0 .. $#points ) {
-    for my $j ( 0 .. $#points ) {
-        $distance{ $i }{ $j } = pow( $points[ $i ][0] - $points[ $j ][0] ) + pow( $points[ $i ][1] - $points[ $j ][1] );
-    }
-}
-
-for my $k ( 0 .. $#points ) {
-    for my $i ( 0 .. $#points ) {
-        for my $j ( 0 .. $#points ) {
-            if ( $distance{ $i }{ $j } > $distance{ $i }{ $k } + $distance{ $k }{ $j } ) {
-                $distance{ $i }{ $j } = $distance{ $i }{ $k } + $distance{ $k }{ $j };
-            }
-        }
-    }
-}
+my %existence;
+$existence{ $_->[0] }{ $_->[1] }++
+    for @points;
 
 my $count = 0;
 
-for my $u ( 0 .. $#points ) {
-    for my $v ( 0 .. $#points ) {
-        for my $w ( 0 .. $#points ) {
-            for my $x ( 0 .. $#points ) {
-                next
-                    if $u == $v || $u == $w || $u == $x || $v == $w || $v == $x || $w == $x;
-                next
-                    if !is_angle( @points[ $u, $v, $w ] ) || !is_angle( @points[ $v, $w, $x ] ) || !is_angle( @points[ $w, $x, $u ] ) || !is_angle( @points[ $x, $u, $v ] );
-                my @distances = (
-                    $distance{ $u }{ $v },
-                    $distance{ $v }{ $w },
-                    $distance{ $w }{ $x },
-                    $distance{ $x }{ $u },
-                );
-                if ( all { $_ == $distances[0] } @distances ) {
-                    #warn "($u, $v, $w, $x)";
-                    #warn "d: ", join q{, }, @distances;
-                    $count++;
-                }
-            }
-        }
+for my $i ( 0 .. $#points ) {
+    for my $j ( 0 .. $#points ) {
+        next
+            if $j == $i;
+
+        $count++
+            if is_square( $i, $j );
     }
 }
 
@@ -64,17 +36,14 @@ say $count / 4;
 
 exit;
 
-sub is_angle {
-    my $a = shift;
-    my $b = shift;
-    my $c = shift;
-    my $ba = [ $a->[0] - $b->[0], $a->[1] - $b->[1] ];
-    my $bc = [ $c->[0] - $b->[0], $c->[1] - $b->[1] ];
-    my $cross = $ba->[0] * $bc->[1] - $ba->[1] * $bc->[0];
-    return $cross > 0;
-}
-
-sub pow {
-    my $value = shift;
-    return $value * $value;
+sub is_square {
+    my( $p1, $p2 ) = @_;
+    my @a = @{ $points[ $p1 ] };
+    my @b = @{ $points[ $p2 ] };
+    my $di = $b[0] - $a[0];
+    my $dj = $b[1] - $a[1];
+    my @c = ( $b[0] - $dj, $b[1] + $di );
+    my @d = ( $c[0] - $di, $c[1] - $dj );
+    return $existence{ $c[0] }{ $c[1] }
+        && $existence{ $d[0] }{ $d[1] };
 }

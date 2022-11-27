2c5328b3122ba8a3202db6a13cc700e5025872c6
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-11-27 20:50:12 +0900

    math algorithm 98: ac

--- math_algorithm/98.pl
+++ math_algorithm/98.pl
@@ -11,35 +11,30 @@ chomp( my $n = <> );
 my @points = map { chomp( my $l = <> ); [ split m{\s}, $l ] }
              1 .. $n;
 my( $x, $y ) = do { chomp( my $l = <> ); split m{\s}, $l };
-$y += 0.5;
+#$y += 1e-9;
 
 push @points, $points[0];
 
-my $count;
+my $count = 0;
 
 for ( my $i = 0; $i < $#points; ++$i ) {
     my $from = $points[ $i ];
     my $to = $points[ $i + 1 ];
 
-    if ( $to->[0] - $from->[0] == 0 ) {
-        if ( $y >= min( $to->[1], $from->[1] ) && $y <= max( $to->[1], $from->[1] ) ) {
-            $count++;
-        }
+    if ( $from->[1] > $to->[1] ) {
+        ( $from, $to ) = ( $to, $from );
+    }
+
+    if ( $y >= $to->[1] || $y < $from->[1] ) {
         next;
     }
 
-    my $a = ( $to->[1] - $from->[1] ) / ( $to->[0] - $from->[0] );
+    my $a = [ $from->[0] - $x, $from->[1] - $y ];
+    my $b = [ $to->[0] - $x, $to->[1] - $y ];
 
-    if ( $a == 0 ) {
-        next;
-    }
+    my $ea = $a->[0] * $b->[1] - $a->[1] * $b->[0];
 
-    my $dy = $y - $from->[1];
-    my $dx = $dy / $a;
-
-    my $cx = $from->[0] + $dx;
-
-    if ( $cx >= $from->[0] && $cx <= $to->[0] ) {
+    if ( $ea < 0 ) {
         $count++;
     }
 }

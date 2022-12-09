de638e42729c54203b67276d5e1189f20c65ad58
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2022-12-09 20:42:06 +0900

    math algorithm 100: ac

--- math_algorithm/100.pl
+++ math_algorithm/100.pl
@@ -13,25 +13,84 @@ my @queries = map { chomp; [ split m{\s} ] }
               1 .. $q;
 
 for my $ref ( @queries ) {
-    printf "%.10f %.10f %.10f\n", brute( @{ $ref } );
+    printf "%.16f %.16f %.16f\n", solve( @{ $ref } );
 }
 
 exit;
 
-sub brute {
+sub solve {
     my( $x, $y, $z, $t ) = @_;
-    my $a = 1;
-    my $b = 1;
-    my $c = 1;
-
-    for my $i ( 1 .. $t ) {
-        my $na = ( 1 - $x ) * $a + $y * $b;
-        my $nb = ( 1 - $y ) * $b + $z * $c;
-        my $nc = ( 1 - $z ) * $c + $x * $a;
-        $a = $na;
-        $b = $nb;
-        $c = $nc;
-    }
-
+    my @a = (
+        [ 1 - $x, $y, 0 ],
+        [ 0, 1 - $y, $z ],
+        [ $x, 0, 1 - $z ],
+    );
+    my $ref = power( \@a, $t );
+    my $a = sum( @{ $ref->[0] } );
+    my $b = sum( @{ $ref->[1] } );
+    my $c = sum( @{ $ref->[2] } );
     return $a, $b, $c;
+}
+
+sub power {
+    my $matrix_ref = shift;
+    my $t = shift;
+    my @matrixes = ( $matrix_ref );
+    my @bits = bits( $t );
+    my $acc = $matrix_ref;
+    for my $i ( 1 .. $#bits ) {
+        $acc = multiply( $acc, $acc );
+        push @matrixes, $acc;
+    }
+    my $result = [
+        [ 1, 0, 0 ],
+        [ 0, 1, 0 ],
+        [ 0, 0, 1 ],
+    ];
+    for my $i ( 0 .. $#bits ) {
+        next
+            unless $bits[ $i ];
+        $result = multiply( $result, $matrixes[ $i ] );
+    }
+    return $result;
+}
+
+sub multiply {
+    my $a = shift;
+    my $b = shift;
+
+    my $c = [
+        [
+            zip_reduce_by_multiply( $a->[0], [ map { $b->[ $_ ][0] } 0 .. 2 ] ),
+            zip_reduce_by_multiply( $a->[0], [ map { $b->[ $_ ][1] } 0 .. 2 ] ),
+            zip_reduce_by_multiply( $a->[0], [ map { $b->[ $_ ][2] } 0 .. 2 ] ),
+        ],
+        [
+            zip_reduce_by_multiply( $a->[1], [ map { $b->[ $_ ][0] } 0 .. 2 ] ),
+            zip_reduce_by_multiply( $a->[1], [ map { $b->[ $_ ][1] } 0 .. 2 ] ),
+            zip_reduce_by_multiply( $a->[1], [ map { $b->[ $_ ][2] } 0 .. 2 ] ),
+        ],
+        [
+            zip_reduce_by_multiply( $a->[2], [ map { $b->[ $_ ][0] } 0 .. 2 ] ),
+            zip_reduce_by_multiply( $a->[2], [ map { $b->[ $_ ][1] } 0 .. 2 ] ),
+            zip_reduce_by_multiply( $a->[2], [ map { $b->[ $_ ][2] } 0 .. 2 ] ),
+        ],
+    ];
+
+    return $c;
+}
+
+sub zip_reduce_by_multiply {
+    my $a = shift;
+    my $b = shift;
+    my $total = 0;
+    for my $i ( 0 .. $#{ $a } ) {
+        $total += $a->[ $i ] * $b->[ $i ];
+    }
+    return $total;
+}
+
+sub bits {
+    my $t = shift;
+    return reverse split m{}, sprintf "%b", $t;
 }

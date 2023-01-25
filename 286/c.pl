bce0c18c158bad537cdd0acb5891972b7bb79767
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-01-25 20:22:48 +0900

    286.c: ac

--- 286/c.pl
+++ 286/c.pl
@@ -5,12 +5,31 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
+use List::Util qw( min );
 
-chomp( my $n = <> );
-my( $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @s = do { chomp( my $l = <> ); split m{\s}, $l };
-my @l = map { chomp; [ split m{\s} ] }
-        map { scalar <> }
-        1 .. $n;
+my( $n, $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
+my @s = do { chomp( my $l = <> ); split m{}, $l };
+
+my $min = $n * $b;
+
+for my $i ( 0 .. $n - 1 ) {
+    my $cost = $i * $a;
+
+    do {
+        my $l = 0;
+        my $r = $#s;
+        while ( $l < $r ) {
+            $cost += $b
+                if $s[ $l ] ne $s[ $r ];
+            $l++;
+            $r--;
+        }
+    };
+
+    $min = min( $min, $cost );
+    push @s, shift @s;
+}
+
+say $min;
 
 exit;

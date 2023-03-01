ca9312f0f32eb08a4e9bfa47fb7f931dc7de3156
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-03-01 19:53:54 +0900

    291.e: wip

--- 291/e.pl
+++ 291/e.pl
@@ -11,62 +11,44 @@ my @xy = map { chomp; [ split m{\s} ] }
          map { scalar <> }
          1 .. $m;
 
-my %has_input;
-$has_input{ $_ }++
-    for 1 .. $n;
-
-my %no_output;
-$no_output{ $_ }++
-    for 1 .. $n;
+my %isnt_max;
 
 my %link;
-my %r_link;
 
 for my $ref ( @xy ) {
     my( $x, $y ) = @{ $ref };
-    delete $has_input{ $y };
-    delete $no_output{ $x };
-    $link{ $x }{ $y }++;
-    $r_link{ $y }{ $x }++;
+    $isnt_max{ $x }++;
+    $link{ $y }{ $x }++;
 }
 
-if ( 1 < %no_output ) {
-    say YesNo::get( 0 );
-    exit;
-}
-
-my @s = keys %has_input;
-
-if ( @s != 1 ) {
-    #warn "could not found start";
-    say YesNo::get( 0 );
-    exit;
-}
-
+my @s = grep { !$isnt_max{ $_ } } 1 .. $n;
 my @l;
 
+if ( !@s ) {
+    say YesNo::get( 0 );
+    exit;
+}
+
 while ( @s ) {
+    if ( @s > 1 ) {
+        say YesNo::get( 0 );
+        exit;
+    }
     my $v = shift @s;
     push @l, $v;
     for my $w ( keys %{ $link{ $v } // { } } ) {
-        delete $link{ $v }{ $w };
-        if ( %{ $r_link{ $w } // { } } == 1 ) {
-            push @s, $w;
+        if ( $isnt_max{ $w } ) {
+            $isnt_max{ $w }--;
+            push @s, $w
+                unless $isnt_max{ $w };
         }
-        delete $link{ $v }
-            unless %{ $link{ $v } };
     }
-}
-
-if ( %link ) {
-    say YesNo::get( 0 );
-    exit;
 }
 
 my @whole = 1 .. $n;
 
 say YesNo::get( 1 );
-#warn join q{ }, @l;
+warn join q{ }, @l;
 say join q{ }, map { $_->[1] } sort { $a->[0] <=> $b->[0] } map { [ $l[ $_ ], $whole[ $_ ] ] } 0 .. $#l;
 
 exit;

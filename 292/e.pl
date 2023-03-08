9299d54372164b753ea7e604666d32690cbdfedd
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-03-08 21:32:29 +0900

    292.e: wip

--- 292/e.pl
+++ 292/e.pl
@@ -6,11 +6,35 @@ use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
 
-chomp( my $n = <> );
-my( $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @s = do { chomp( my $l = <> ); split m{\s}, $l };
-my @l = map { chomp; [ split m{\s} ] }
-        map { scalar <> }
-        1 .. $n;
+my( $n, $m ) = do { chomp( my $l = <> ); split m{\s}, $l };
+my @edges = map { chomp; [ split m{\s} ] }
+            map { scalar <> }
+            1 .. $m;
+
+my %links;
+
+for my $ref ( @edges ) {
+    my( $from, $to ) = @{ $ref };
+    push @{ $links{ $from } }, $to;
+}
+
+my $total = 0;
+
+for my $v ( 1 .. $n ) {
+    my %visited;
+    my @queue = @{ $links{ $v } // [ ] };
+
+    while ( @queue ) {
+        my $w = shift @queue;
+        next
+            if $visited{ $w }++;
+        push @queue, grep { !$visited{ $_ } } @{ $links{ $w } // [ ] };
+    }
+
+    $total += %visited - @{ $links{ $v } // [ ] };
+}
+
+say $total;
+
 
 exit;

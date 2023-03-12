04e1ee1c90580aedd547923083ca619a57f865ab
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-03-12 20:36:41 +0900

    beam_search: chokudai_search

--- searching_algorithm/beam_search.pl
+++ searching_algorithm/beam_search.pl
@@ -13,8 +13,58 @@ $s->dump;
 
 #say random( $s, 92, 1000 ); # 12.754
 #say beam_search( $s, 92, 1000, 4, 100 ); # 23
+say chokudai_search( $s, 92, 100, 4, 1, 2 ); # 23
 
 exit;
+
+sub chokudai_search {
+    my $base = shift;
+    my $seed = shift;
+    my $playout = shift;
+    my $beam_depth = shift;
+    my $beam_width = shift;
+    my $beam_size = shift;
+    srand $seed;
+
+    my $total = 0;
+    my @chokudai_beams = map { { set => { }, beams => [ ] } } 0 .. $beam_depth;
+    $chokudai_beams[0] = { set => { q{} => 1 }, beams => [ { operations => [ ], state => $base->clone } ] };
+
+    for my $p ( 0 .. $playout - 1 ) {
+        for my $i ( 0 .. $beam_size - 1 ) {
+            for my $j ( 0 .. $beam_depth - 1 ) {
+                my @current_beams = @{ $chokudai_beams[ $j ]{beams} };
+                my %next_operation = %{ $chokudai_beams[ $j + 1 ]{set} };
+                my @next_beams = @{ $chokudai_beams[ $j + 1 ]{beams} };
+
+                for my $k ( 0 .. min( $beam_width - 1, $#current_beams ) ) {
+                    my %h = %{ shift @current_beams };
+
+                    for my $action ( $h{state}->valid_actions ) {
+                        next
+                            if $next_operation{ get_operations_hash( $h{operations}, $action ) }++;
+                        my $state = $h{state}->clone;
+                        $state->advance( $action );
+                        push @next_beams, { operations => [ @{ $h{operations} }, $action ], state => $state };
+                    }
+                }
+
+                $chokudai_beams[ $j + 1 ]{beams} = [ sort { $b->{state}->score <=> $a->{state}->score } @next_beams ];
+                $#{ $chokudai_beams[ $j + 1 ]{beams} } = min( $#{ $chokudai_beams[ $j + 1 ]{beams} }, $beam_width );
+            }
+        }
+
+        $total += $chokudai_beams[ $beam_depth ]{beams}[0]{state}->score;
+    }
+
+    return $total / $playout;
+}
+
+sub get_operations_hash {
+    my $operations_ref = shift;
+    my $new = shift;
+    return join q{:}, map { join q{,}, @{ $_ } } ( @{ $operations_ref }, $new );
+}
 
 sub beam_search {
     my $base = shift;
@@ -77,12 +127,12 @@ package TimeLimit {
     sub new {
         my $class = shift;
         my $limit = shift;
-        return bless { began_at => [ gettimeofday ] }, $class;
+        return bless { began_at => [ gettimeofday ], limit => $limit }, $class;
     }
 
     sub did_exceeded {
         my $self = shift;
-        return tv_interval( $self->{began_at}, [ gettimeofday ] ) > $limit;
+        return tv_interval( $self->{began_at}, [ gettimeofday ] ) > $self->{limit};
     }
 }
 
@@ -130,6 +180,8 @@ package State {
         my $action_ref = shift;
         die "already done"
             if $self->is_done;
+        die "invalid action: ", Dumper $action_ref
+            if ref $action_ref ne ref [ ];
         my @coord = ( $self->{character}[0] + $action_ref->[0], $self->{character}[1] + $action_ref->[1] );
         die "invalid action"
             if $coord[0] < 0 || $coord[0] >= $self->height || $coord[1] < 0 || $coord[1] >= $self->width;

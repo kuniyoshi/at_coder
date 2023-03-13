cecc6e6b32e06f7f924956333e476fc803be1602
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-03-13 20:27:10 +0900

    293.c: alternative

--- 293/c.pl
+++ 293/c.pl
@@ -11,19 +11,30 @@ my @a = map { chomp; [ split m{\s} ] }
         map { scalar <> }
         1 .. $h;
 
-say dfs( [ 0, 0 ], ( ) );
+say dfs( [ 0, 0 ], { } );
 
 exit;
 
 sub dfs {
     my $coord_ref = shift;
-    my %owned = @_;
+    my $owned_ref = shift;
     my( $i, $j ) = @{ $coord_ref };
-    return 0
-        if $owned{ $a[ $i ][ $j ] }++;
-    return 1
-        if $i == $h - 1 && $j == $w - 1;
-    my $left = $j != $w - 1 ? dfs( [ $i, $j + 1 ], %owned ) : 0;
-    my $right = $i != $h - 1 ? dfs( [ $i + 1, $j ], %owned ) : 0;
+
+    if ( $owned_ref->{ $a[ $i ][ $j ] } ) {
+        return 0;
+    }
+
+    $owned_ref->{ $a[ $i ][ $j ] }++;
+
+    if ( $i == $h - 1 && $j == $w - 1 ) {
+        delete $owned_ref->{ $a[ $i ][ $j ] };
+        return 1;
+    }
+
+    my $left = $j != $w - 1 ? dfs( [ $i, $j + 1 ], $owned_ref ) : 0;
+    my $right = $i != $h - 1 ? dfs( [ $i + 1, $j ], $owned_ref ) : 0;
+
+    delete $owned_ref->{ $a[ $i ][ $j ] };
+
     return $left + $right;
 }

3cc71fd3232accc85759f58b0ce85ef17424f1fc
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-03-16 21:00:18 +0900

    293.e: pow can, but...

--- 293/e.pl
+++ 293/e.pl
@@ -6,12 +6,17 @@ use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
 
-say f( 3, 0 );
-say f( 3, 1 );
-say f( 3, 2 );
-say f( 3, 3 );
-say f( 3, 4 );
-say f( 3, 5 );
+my( $a, $x, $m ) = do { chomp( my $l = <> ); split m{\s}, $l };
+
+my $total = 0;
+
+for my $i ( 0 .. $x - 1 ) {
+    $total = ( $total + f( $a, $i ) ) % $m;
+}
+
+say $total;
+
+exit;
 
 sub f {
     my $x = shift;
@@ -22,37 +27,12 @@ sub f {
         if ( $y & 1 ) {
             $a *= $x;
         }
-        else {
-            $a *= $x;
-        }
+        $x *= $x;
         $y >>= 1;
     }
 
     return $a;
 }
-
-__END__
-my $x = 4;
-my $m = 700;
-
-say pow( 3, 0 );
-say pow( 3, 1 );
-say pow( 3, 2 );
-say pow( 3, 3 );
-
-exit;
-
-#my( $a, $x, $m ) = do { chomp( my $l = <> ); split m{\s}, $l };
-
-my $total = 0;
-
-for my $i ( 0 .. $x - 1 ) {
-    $total = ( $total + pow( $a, $i ) ) % $m;
-}
-
-say $total;
-
-exit;
 
 sub pow {
     my( $a, $b ) = @_;

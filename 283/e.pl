4d99c3bf128bf5b7516bf15aca6d4600acefee8f
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-03-20 20:20:14 +0900

    283.e: wip

--- 283/e.pl
+++ 283/e.pl
@@ -6,11 +6,13 @@ use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
 
-chomp( my $n = <> );
-my( $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @s = do { chomp( my $l = <> ); split m{\s}, $l };
-my @l = map { chomp; [ split m{\s} ] }
+my( $h, $w ) = do { chomp( my $l = <> ); split m{\s}, $l };
+my @a = map { chomp; [ split m{\s} ] }
         map { scalar <> }
-        1 .. $n;
+        1 .. $h;
+
+my @dp;
+
+for my $i ( 
 
 exit;

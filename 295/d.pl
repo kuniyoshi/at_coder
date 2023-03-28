12731a46344acabd775b9aa54ac1375803df3eaf
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-03-28 19:57:59 +0900

    295.d: wip

--- 295/d.pl
+++ 295/d.pl
@@ -6,11 +6,28 @@ use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
 
-chomp( my $n = <> );
-my( $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @s = do { chomp( my $l = <> ); split m{\s}, $l };
-my @l = map { chomp; [ split m{\s} ] }
-        map { scalar <> }
-        1 .. $n;
+my @s = do { chomp( my $l = <> ); split m{}, $l };
+
+my @accs = map { [ 0 ] } 0 .. 9;
+
+for my $i ( 0 .. $#s ) {
+    for my $j ( 0 .. 9 ) {
+        $accs[ $j ][ $i + 1 ] = ( $accs[ $j ][ $i ] + ( $j == $s[ $i ] ) ) % 2;
+    }
+}
+
+#say join q{, }, @{ $_ }
+#    for @accs;
+#die;
+
+my @values;
+
+for my $i ( 0 .. @s ) {
+    $values[ $i ] = 0;
+    $values[ $i ] += $accs[ $_ ][ $i ] << $_
+        for 0 .. 9;
+}
+
+die Dumper \@values;
 
 exit;

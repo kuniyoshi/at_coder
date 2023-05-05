fba9a44b99ad2ba7442090b956b44b42a9979261
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-05-05 21:28:57 +0900

    125.c: wip

--- 125/c.pl
+++ 125/c.pl
@@ -5,10 +5,55 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
-use List::Util qw( min );
+use List::Util qw( max );
 
 chomp( my $n = <> );
 my @a = do { chomp( my $l = <> ); split m{\s}, $l };
+
+my @l = ( $a[0] );
+
+for my $i ( 1 .. $n - 1 ) {
+    push @l, gcd( $a[ $i ], $l[ $i - 1 ] );
+}
+
+my @r = ( $a[-1] );
+
+for my $i ( reverse 0 .. $n - 2 ) {
+    push @r, gcd( $a[ $i ], $a[ $i + 1 ] );
+}
+
+@r = reverse @r;
+
+my $max = 1;
+
+for my $i ( 0 .. $n - 1 ) {
+    if ( $i == 0 ) {
+        $max = max( $max, $r[1] );
+        next;
+    }
+
+    if ( $i == $n - 1 ) {
+        $max = max( $max, $l[0] );
+        next;
+    }
+
+    warn "gcd( $l[ $i ], $r[ -( $i + 1 ) ] ): ", gcd( $l[ $i ], $r[ -( $i + 1 ) ] );
+    $max = max( $max, gcd( $l[ $i - 1 ], $r[ $i + 1 ] ) );
+}
+
+say $max;
+
+exit;
+
+sub gcd {
+    my( $a, $b ) = @_;
+    while ( $b ) {
+        ( $a, $b ) = ( $b, $a % $b );
+    }
+    return $a;
+}
+
+__END__
 my @primes = list_primes( );
 my @cds;
 expand( $a[ $_ ] , ( $cds[ $_ ] = { } ) )

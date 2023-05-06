e377770f8902e89c86e33708f8b8f71c70355bce
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-05-06 20:05:43 +0900

    125.c: wa

--- 125/c.pl
+++ 125/c.pl
@@ -33,11 +33,11 @@ for my $i ( 0 .. $n - 1 ) {
     }
 
     if ( $i == $n - 1 ) {
-        $max = max( $max, $l[0] );
+        $max = max( $max, $l[ $i ] );
         next;
     }
 
-    warn "gcd( $l[ $i ], $r[ -( $i + 1 ) ] ): ", gcd( $l[ $i ], $r[ -( $i + 1 ) ] );
+    #    warn "gcd( $l[ $i ], $r[ -( $i + 1 ) ] ): ", gcd( $l[ $i ], $r[ -( $i + 1 ) ] );
     $max = max( $max, gcd( $l[ $i - 1 ], $r[ $i + 1 ] ) );
 }
 
@@ -51,67 +51,4 @@ sub gcd {
         ( $a, $b ) = ( $b, $a % $b );
     }
     return $a;
-}
-
-__END__
-my @primes = list_primes( );
-my @cds;
-expand( $a[ $_ ] , ( $cds[ $_ ] = { } ) )
-    for 0 .. $#a;
-
-my $result = 1;
-my $worked;
-
-for my $prime ( reverse @primes ) {
-    my $owners = 0;
-    for my $i ( 0 .. $#a ) {
-        $owners++
-            if $cds[ $i ]{ $prime };
-    }
-    if ( !$worked && $owners == $n - 1 ) {
-        $worked++;
-        $result *= $prime;
-        next;
-    }
-    if ( $owners == $n ) {
-        $result *= $prime ** min( map { $_->{ $prime } } @cds );
-    }
-}
-
-say $result;
-
-exit;
-
-sub expand {
-    my $v = shift;
-    my $ref = shift;
-    for my $prime ( @primes ) {
-        return
-            if $prime > $v;
-        while ( $v % $prime == 0 ) {
-            $ref->{ $prime }++;
-            $v /= $prime;
-        }
-    }
-}
-
-sub list_primes {
-    my $max = int( sqrt( 1e9 ) );
-    my @is_prime = ( 1 ) x ( $max + 1 );
-
-    $is_prime[0] = 0;
-    $is_prime[1] = 0;
-
-    my @primes;
-
-    for my $i ( 2 .. $#is_prime ) {
-        next
-            unless $is_prime[ $i ];
-        push @primes, $i;
-        for ( my $j = $i + $i; $j <= @is_prime; $j += $i ) {
-            $is_prime[ $j ] = 0;
-        }
-    }
-
-    return @primes;
 }

6fa08fa627f7929b2a44a2bd7bc6a6310c467110
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-05-07 18:24:00 +0900

    125.d: ac

--- 125/d.pl
+++ 125/d.pl
@@ -5,12 +5,24 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
+use List::Util qw( max );
 
 chomp( my $n = <> );
-my( $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @s = do { chomp( my $l = <> ); split m{\s}, $l };
-my @l = map { chomp; [ split m{\s} ] }
-        map { scalar <> }
-        1 .. $n;
+my @a = do { chomp( my $l = <> ); split m{\s}, $l };
+
+my @dp = ( [ $a[0] ], [ -$a[0] ] );
+
+for my $i ( 1 .. $n - 1 ) {
+    $dp[0][ $i ] = max(
+        $dp[0][ $i - 1 ] + $a[ $i ],
+        $dp[1][ $i - 1 ] - $a[ $i ],
+    );
+    $dp[1][ $i ] = max(
+        $dp[0][ $i - 1 ] - $a[ $i ],
+        $dp[1][ $i - 1 ] + $a[ $i ],
+    );
+}
+
+say $dp[0][ $n - 1 ];
 
 exit;

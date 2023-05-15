0d8d4de05b93decd33ad7e58d9e51b59c82e0e9c
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-05-15 21:11:53 +0900

    300.e: wip

--- 300/e.pl
+++ 300/e.pl
@@ -6,11 +6,69 @@ use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
 
-chomp( my $n = <> );
-my( $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @s = do { chomp( my $l = <> ); split m{\s}, $l };
-my @l = map { chomp; [ split m{\s} ] }
-        map { scalar <> }
-        1 .. $n;
+#chomp( my $n = <> );
+#my( $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
+#my @s = do { chomp( my $l = <> ); split m{\s}, $l };
+#my @l = map { chomp; [ split m{\s} ] }
+#        map { scalar <> }
+#        1 .. $n;
+my %factor = factor( 1e18 );
+my $mod = Mod->new;
 
 exit;
+
+sub factor {
+    my $n = shift;
+    my %factor;
+
+    for ( my $i = 2; $i * $i <= $n; ++$i ) {
+        next
+            if $n % $i;
+        my $count = 0;
+        while ( $n % $i == 0 ) {
+            $n /= $i;
+            $count++;
+        }
+        $factor{ $i } = $count;
+    }
+
+    return %factor;
+}
+
+package Mod {
+    sub mod { 998244353 }
+
+    sub new {
+        my $class = shift;
+        my $value = shift // 0;
+        return bless \$value, $class;
+    }
+
+    sub value {
+        return ${ shift( ) };
+    }
+
+    sub add {
+        my $self = shift;
+        my $value = shift;
+        ${ $self } += $value;
+        ${ $self } %= mod( );
+        return $self;
+    }
+
+    sub subtract {
+        my $self = shift;
+        my $value = shift;
+        ${ $self } -= $value;
+        ${ $self } %= mod( );
+        return $self;
+    }
+
+    sub multiply {
+        my $self = shift;
+        my $value = shift;
+        ${ $self } *= $value;
+        ${ $self } %= mod( );
+        return $self;
+    }
+}

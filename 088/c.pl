d0071fc4bdbe5635254c0ac22fdac0ef6bc9df9a
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-05-27 12:14:12 +0900

    088.c: ac

--- 088/c.pl
+++ 088/c.pl
@@ -10,11 +10,55 @@ my @c = map { chomp; [ split m{\s} ] }
         map { scalar <> }
         1 .. 3;
 
+SEARCH:
+for my $a1 ( 0 .. 100 ) {
+    my @b = map { $c[0][ $_ ] - $a1 } 0 .. 2;
+    my $a2 = $c[1][0] - $b[0];
+    my $a3 = $c[2][0] - $b[0];
+    my @a = ( $a1, $a2, $a3 );
+
+    for my $i ( 0 .. 2 ) {
+        for my $j ( 0 .. 2 ) {
+            next SEARCH
+                if $a[ $i ] + $b[ $j ] != $c[ $i ][ $j ];
+        }
+    }
+
+    say YesNo::get( 1 );
+    exit;
+}
+
+say YesNo::get( 0 );
 
 exit;
 
+use utf8;
+use strict;
+use warnings;
+
+package YesNo;
+
+sub get {
+    return $_[-1] ? "Yes" : "No";
+}
+
+1;
 __END__
-2
+c11 = a1 + b1
+c12 = a1 + b2
+c13 = a1 + b3
+c21 = a2 + b1
+c22 = a2 + b2
+c23 = a2 + b3
+c31 = a3 + b1
+c32 = a3 + b2
+c33 = a3 + b3
+
+c12 - c11 = b2 - b1
+c13 - c11 = b3 - b1
+c13 - c12 = b3 - b2
+
+c13 - c11 = b3 - ( -( c12 - c11 ) + b2 )
 
 a1 + b1     a1 + b2     a1 + b3 = 3 a1 + b1 + b2 + b3
 a2 + b1     a2 + b2     a2 + b3 = 3 a2 + b1 + b2 + b3

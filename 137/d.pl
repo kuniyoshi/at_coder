aeefe9b2bc40f60fa861848872fbb01f48df2afc
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-06-04 15:23:13 +0900

    137.d: ac

--- 137/d.pl
+++ 137/d.pl
@@ -8,27 +8,184 @@ use Data::Dumper;
 use List::Util qw( min );
 
 my( $n, $m ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @jobs = sort { $b->[1] <=> $a->[1] }
-           sort { $b->[0] <=> $a->[0] }
+my @jobs = sort { $a->[0] <=> $b->[0] }
            map { chomp; [ split m{\s} ] }
            map { scalar <> }
            1 .. $n;
 
 my $money = 0;
-my $min_days = $m + 1;
+my $queue = PriorityQueue::PriorLarge->new;
 
-while ( @jobs && $m ) {
-    shift @jobs
-        while @jobs && $jobs[0][0] > $min_days;
-    last
-        if !@jobs;
-    warn "($jobs[0][0], $jobs[0][1])";
-    $min_days = min( $min_days, $jobs[0][0] );
-    $m--;
-    $money += $jobs[0][1];
-    shift @jobs;
+for my $passed ( reverse 0 .. $m - 1 ) {
+    my $remain = $m - $passed;
+    #warn "\$remain: $remain";
+    while ( @jobs && $jobs[0][0] <= $remain ) {
+        my $job = shift @jobs;
+        #        warn "($job->[0], $job->[1])";
+        $queue->push( $job->[1] );
+    }
+    next
+        unless $queue->size;
+    $money += $queue->pop;
 }
 
 say $money;
 
 exit;
+
+use strict;
+use warnings;
+
+package PriorityQueue::PriorSmall;
+
+sub new {
+    my $class = shift;
+    return bless { items => [ ] }, $class;
+}
+
+sub dump {
+    my $self = shift;
+    return join q{, }, @{ $self->{items} };
+}
+
+sub push {
+    my $self = shift;
+    my $value = shift;
+    Heap::reverse_push_to( $self->{items}, $value );
+}
+
+sub pop {
+    my $self = shift;
+
+    die "Could not pop while buffer is empty"
+        unless @{ $self->{items} };
+
+    return Heap::reverse_pop_from( $self->{items} );
+}
+
+sub peek {
+    return shift->{items}[0];
+}
+
+sub size {
+    return scalar @{ shift->{items} };
+}
+
+package PriorityQueue::PriorLarge;
+
+sub new {
+    my $class = shift;
+    return bless { items => [ ] }, $class;
+}
+
+sub dump {
+    my $self = shift;
+    return join q{, }, @{ $self->{items} };
+}
+
+sub push {
+    my $self = shift;
+    my $value = shift;
+    Heap::push_to( $self->{items}, $value );
+}
+
+sub pop {
+    my $self = shift;
+
+    die "Could not pop while buffer is empty"
+        unless @{ $self->{items} };
+
+    return Heap::pop_from( $self->{items} );
+}
+
+sub peek {
+    return shift->{items}[0];
+}
+
+sub size {
+    return @{ shift->{items} };
+}
+
+package Heap;
+
+sub push_to {
+    my( $buffer_ref, $item ) = @_;
+
+    push @{ $buffer_ref }, $item;
+
+    my $cursor = $#{ $buffer_ref };
+
+    while ( $cursor != 0 ) {
+        my $parent = int( ( $cursor - 1 ) / 2 );
+        @{ $buffer_ref }[ $parent, $cursor ] = @{ $buffer_ref }[ $cursor, $parent ]
+            if $buffer_ref->[ $parent ] <= $buffer_ref->[ $cursor ];
+        $cursor = $parent;
+    }
+}
+
+sub pop_from {
+    my $buffer_ref = shift;
+
+    my $last_root = $buffer_ref->[0];
+    $buffer_ref->[0] = $buffer_ref->[-1];
+    $#{ $buffer_ref }--;
+
+    my $cursor = 0;
+
+    while ( ( my $left = 2 * $cursor + 1 ) < @{ $buffer_ref } ) {
+        my $right = $left + 1;
+
+        my $child = $right < @{ $buffer_ref } && $buffer_ref->[ $left ] <= $buffer_ref->[ $right ]
+            ? $right
+            : $left;
+
+        @{ $buffer_ref }[ $cursor, $child ] = @{ $buffer_ref }[ $child, $cursor ]
+            if $buffer_ref->[ $cursor ] <= $buffer_ref->[ $child ];
+
+        $cursor = $child;
+    }
+
+    return $last_root;
+}
+
+sub reverse_push_to {
+    my( $buffer_ref, $item ) = @_;
+
+    push @{ $buffer_ref }, $item;
+
+    my $cursor = $#{ $buffer_ref };
+
+    while ( $cursor != 0 ) {
+        my $parent = int( ( $cursor - 1 ) / 2 );
+        @{ $buffer_ref }[ $parent, $cursor ] = @{ $buffer_ref }[ $cursor, $parent ]
+            if $buffer_ref->[ $parent ] >= $buffer_ref->[ $cursor ];
+        $cursor = $parent;
+    }
+}
+
+sub reverse_pop_from {
+    my $buffer_ref = shift;
+
+    my $last_root = $buffer_ref->[0];
+    $buffer_ref->[0] = $buffer_ref->[-1];
+    $#{ $buffer_ref }--;
+
+    my $cursor = 0;
+
+    while ( ( my $left = 2 * $cursor + 1 ) < @{ $buffer_ref } ) {
+        my $right = $left + 1;
+
+        my $child = $right < @{ $buffer_ref } && $buffer_ref->[ $left ] >= $buffer_ref->[ $right ]
+            ? $right
+            : $left;
+
+        @{ $buffer_ref }[ $cursor, $child ] = @{ $buffer_ref }[ $child, $cursor ]
+            if $buffer_ref->[ $cursor ] >= $buffer_ref->[ $child ];
+
+        $cursor = $child;
+    }
+
+    return $last_root;
+}
+
+1;

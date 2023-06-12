86048518a7194de9f0397f80dbde2a550abcbe89
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-06-12 20:37:03 +0900

    111.c: ac

--- 111/c.pl
+++ 111/c.pl
@@ -21,17 +21,16 @@ for my $i ( 0 .. $n - 1 ) {
 my @odd_counts =  map { [ $_, $odd{ $_ }  ] } sort { $odd{ $b }  <=> $odd{ $a }  } keys %odd;
 my @even_counts = map { [ $_, $even{ $_ } ] } sort { $even{ $b } <=> $even{ $a } } keys %even;
 
+push @odd_counts, [ undef, 0 ];
+push @even_counts, [ undef, 0 ];
+
 my $keeps = do {
-    my( $greater_ref, $fewer_ref ) = $odd_counts[0][1] >= $even_counts[0][1]
-        ? ( \@odd_counts, \@even_counts )
-        : ( \@even_counts, \@odd_counts );
-warn "is same: ", $fewer_ref->[0][0] == $greater_ref->[0][0];
+    my( $greater_ref, $fewer_ref ) = align( \@odd_counts, \@even_counts );
+    #warn "is same: ", $fewer_ref->[0][0] == $greater_ref->[0][0];
     shift @{ $fewer_ref }
         if $fewer_ref->[0][0] == $greater_ref->[0][0];
-    $fewer_ref = [ [ undef, 0 ] ]
-       unless @{ $fewer_ref };
-warn $greater_ref->[0][1];
-warn $fewer_ref->[0][1];
+        #warn $greater_ref->[0][1];
+        #warn $fewer_ref->[0][1];
     $greater_ref->[0][1] + $fewer_ref->[0][1];
 };
 
@@ -39,3 +38,15 @@ say $n - $keeps;
 
 exit;
 
+sub align {
+    my $a_ref = shift;
+    my $b_ref = shift;
+    if ( $a_ref->[0][1] == $b_ref->[0][1] ) {
+        return $a_ref->[1][1] >= $b_ref->[1][1]
+            ? ( $b_ref, $a_ref )
+            : ( $a_ref, $b_ref );
+    }
+    return $a_ref->[0][1] >= $b_ref->[0][1]
+        ? ( $a_ref, $b_ref )
+        : ( $b_ref, $a_ref );
+}

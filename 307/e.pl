b34f201813707eeea7e30fabdd487a1b4fbf10dc
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-07-26 19:56:46 +0900

    307.e: wip

--- 307/e.pl
+++ 307/e.pl
@@ -6,11 +6,25 @@ use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
 
-chomp( my $n = <> );
-my( $a, $b ) = do { chomp( my $l = <> ); split m{\s}, $l };
-my @s = do { chomp( my $l = <> ); split m{\s}, $l };
-my @l = map { chomp; [ split m{\s} ] }
-        map { scalar <> }
-        1 .. $n;
+my $MOD = 998244353;
+
+my( $n, $m ) = do { chomp( my $l = <> ); split m{\s}, $l };
+
+my @dp = map { [ ( $_ == 1 ) x $m ] } 1 .. $n;
+
+for ( my $i = 1; $i < $n; ++$i ) {
+    for ( my $j = 0; $j < $m; ++$j ) {
+        for ( my $k = 0; $k < $m; ++$k ) {
+            next
+                if $k == $i;
+            $dp[ $i ][ $j ] += $dp[ $i - 1 ][ $k ];
+            $dp[ $i ][ $j ] %= $MOD;
+        }
+    }
+}
+
+die Dumper \@dp;
+say $dp[ $n - 1 ][ $m - 1 ];
+
 
 exit;

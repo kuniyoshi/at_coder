25aa6b227ba9581d7dc5752cd7c0b9ec3b8ff8cf
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-08-09 21:15:54 +0900

    311.e: wip

--- 311/e.pl
+++ 311/e.pl
@@ -11,29 +11,24 @@ my @ab = map { chomp; [ split m{\s} ] }
          map { scalar <> }
          1 .. $n;
 
-my %is_hole;
-
-for my $ab ( @ab ) {
-    my( $i, $j ) = @{ $ab };
-    $is_hole{ $i }{ $j }++;
-}
-
 my @acc = map { [ ( 0 ) x ( $w + 1 ) ] }
           1 .. $h + 1;
 
+for my $ab ( @ab ) {
+    my( $i, $j ) = @{ $ab };
+    $acc[ $i ][ $j ] = 1;
+}
+
+
 for my $i ( 1 .. $h ) {
-    my $acc = 0;
     for my $j ( 1 .. $w ) {
-        $acc += $acc[ $i ][ $j - 1 ] + ( $is_hole{ $i }{ $j } ? 0 : 1 );
-        $acc[ $i ][ $j ] = $acc;
+        $acc[ $i ][ $j ] += $acc[ $i ][ $j - 1 ];
     }
 }
 
 for my $j ( 1 .. $w ) {
-    my $acc = 0;
     for my $i ( 1 .. $h ) {
-        $acc += $acc[ $i - 1 ][ $j ];
-        $acc[ $i ][ $j ] += $acc;
+        $acc[ $i ][ $j ] += $acc[ $i - 1 ][ $j ];
     }
 }
 

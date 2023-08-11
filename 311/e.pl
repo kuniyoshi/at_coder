7bad4343611bd5f2dd4a4d8e79ab22ef5453950f
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-08-11 13:25:44 +0900

    311.e: tle

--- 311/e.pl
+++ 311/e.pl
@@ -5,11 +5,16 @@ use strict;
 use warnings;
 use open qw( :utf8 :std );
 use Data::Dumper;
+use List::Util qw( min );
 
 my( $h, $w, $n ) = do { chomp( my $l = <> ); split m{\s}, $l };
 my @ab = map { chomp; [ split m{\s} ] }
          map { scalar <> }
          1 .. $n;
+
+my %is_hole;
+$is_hole{ $_->[0] }{ $_->[1] }++
+    for @ab;
 
 my @acc = map { [ ( 0 ) x ( $w + 1 ) ] }
           1 .. $h + 1;
@@ -32,6 +37,47 @@ for my $j ( 1 .. $w ) {
     }
 }
 
-die map { join( qq{\t}, @{ $_ } ), "\n" } @acc;
+#warn map { join( qq{\t}, @{ $_ } ), "\n" } @acc;
+
+my $max_length = min( $h, $w );
+
+my $count = 0;
+
+for my $i ( 1 .. $h ) {
+    for my $j ( 1 .. $w ) {
+        next
+            if $is_hole{ $i }{ $j };
+        my $wa = $max_length;
+        my $ac = 0;
+
+        while ( $wa - $ac > 1 ) {
+            my $wj = int( ( $wa + $ac ) / 2 );
+            my $can_be = $wj + $i <= $h && $wj + $j <= $w;
+
+            if ( !$can_be ) {
+                $wa = $wj;
+                next;
+            }
+
+            my $holes = $acc[ $i + $wj ][ $j + $wj ]
+                + $acc[ $i ][ $j ]
+                - $acc[ $i - 1 ][ $j + $wj ]
+                - $acc[ $i + $wj ][ $j - 1 ];
+
+            if ( !$holes ) {
+                $ac = $wj;
+            }
+            else {
+                $wa = $wj;
+            }
+        }
+
+        #        warn "( $i, $j ) -> ", $ac + 1;
+
+        $count += $ac + 1;
+    }
+}
+
+say $count;
 
 exit;

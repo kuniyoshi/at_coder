012094c4b99fcc7fb23726e15e275f184fe86bed
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-10-15 15:45:55 +0900

    tessoku b39: ac

---
+++ tessoku_book/b39.pl
@@ -0,0 +1,188 @@
+#!/usr/bin/env perl
+use 5.10.0;
+use utf8;
+use strict;
+use warnings;
+use open qw( :utf8 :std );
+use Data::Dumper;
+
+my( $n, $d ) = do { chomp( my $l = <> ); split m{\s}, $l };
+my @jobs = sort { $a->[0] <=> $b->[0] }
+           map { chomp; [ split m{\s} ] }
+           map { scalar <> }
+           1 .. $n;
+
+my $queue = PriorityQueue::PriorLarge->new;
+my $total = 0;
+
+for ( my $i = 0; $i < $d; ++$i ) {
+    while ( @jobs && $jobs[0][0] <= $i + 1 ) {
+        my $job = shift @jobs;
+        $queue->push( $job->[1] );
+    }
+
+    if ( $queue->size ) {
+        $total += $queue->pop;
+    }
+}
+
+say $total;
+
+exit;
+
+use strict;
+use warnings;
+
+package PriorityQueue::PriorSmall;
+
+sub new {
+    my $class = shift;
+    return bless { items => [ ] }, $class;
+}
+
+sub dump {
+    my $self = shift;
+    return join q{, }, @{ $self->{items} };
+}
+
+sub push {
+    my $self = shift;
+    my $value = shift;
+    Heap::reverse_push_to( $self->{items}, $value );
+}
+
+sub pop {
+    my $self = shift;
+
+    die "Could not pop while buffer is empty"
+        unless @{ $self->{items} };
+
+    return Heap::reverse_pop_from( $self->{items} );
+}
+
+sub peek {
+    return shift->{items}[0];
+}
+
+sub size {
+    return scalar @{ shift->{items} };
+}
+
+package PriorityQueue::PriorLarge;
+
+sub new {
+    my $class = shift;
+    return bless { items => [ ] }, $class;
+}
+
+sub dump {
+    my $self = shift;
+    return join q{, }, @{ $self->{items} };
+}
+
+sub push {
+    my $self = shift;
+    my $value = shift;
+    Heap::push_to( $self->{items}, $value );
+}
+
+sub pop {
+    my $self = shift;
+
+    die "Could not pop while buffer is empty"
+        unless @{ $self->{items} };
+
+    return Heap::pop_from( $self->{items} );
+}
+
+sub peek {
+    return shift->{items}[0];
+}
+
+sub size {
+    return @{ shift->{items} };
+}
+
+package Heap;
+
+sub push_to {
+    my( $buffer_ref, $item ) = @_;
+
+    push @{ $buffer_ref }, $item;
+
+    my $cursor = $#{ $buffer_ref };
+
+    while ( $cursor != 0 ) {
+        my $parent = int( ( $cursor - 1 ) / 2 );
+        @{ $buffer_ref }[ $parent, $cursor ] = @{ $buffer_ref }[ $cursor, $parent ]
+            if $buffer_ref->[ $parent ] <= $buffer_ref->[ $cursor ];
+        $cursor = $parent;
+    }
+}
+
+sub pop_from {
+    my $buffer_ref = shift;
+
+    my $last_root = $buffer_ref->[0];
+    $buffer_ref->[0] = $buffer_ref->[-1];
+    $#{ $buffer_ref }--;
+
+    my $cursor = 0;
+
+    while ( ( my $left = 2 * $cursor + 1 ) < @{ $buffer_ref } ) {
+        my $right = $left + 1;
+
+        my $child = $right < @{ $buffer_ref } && $buffer_ref->[ $left ] <= $buffer_ref->[ $right ]
+            ? $right
+            : $left;
+
+        @{ $buffer_ref }[ $cursor, $child ] = @{ $buffer_ref }[ $child, $cursor ]
+            if $buffer_ref->[ $cursor ] <= $buffer_ref->[ $child ];
+
+        $cursor = $child;
+    }
+
+    return $last_root;
+}
+
+sub reverse_push_to {
+    my( $buffer_ref, $item ) = @_;
+
+    push @{ $buffer_ref }, $item;
+
+    my $cursor = $#{ $buffer_ref };
+
+    while ( $cursor != 0 ) {
+        my $parent = int( ( $cursor - 1 ) / 2 );
+        @{ $buffer_ref }[ $parent, $cursor ] = @{ $buffer_ref }[ $cursor, $parent ]
+            if $buffer_ref->[ $parent ] >= $buffer_ref->[ $cursor ];
+        $cursor = $parent;
+    }
+}
+
+sub reverse_pop_from {
+    my $buffer_ref = shift;
+
+    my $last_root = $buffer_ref->[0];
+    $buffer_ref->[0] = $buffer_ref->[-1];
+    $#{ $buffer_ref }--;
+
+    my $cursor = 0;
+
+    while ( ( my $left = 2 * $cursor + 1 ) < @{ $buffer_ref } ) {
+        my $right = $left + 1;
+
+        my $child = $right < @{ $buffer_ref } && $buffer_ref->[ $left ] >= $buffer_ref->[ $right ]
+            ? $right
+            : $left;
+
+        @{ $buffer_ref }[ $cursor, $child ] = @{ $buffer_ref }[ $child, $cursor ]
+            if $buffer_ref->[ $cursor ] >= $buffer_ref->[ $child ];
+
+        $cursor = $child;
+    }
+
+    return $last_root;
+}
+
+1;

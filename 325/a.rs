95800e16a6f5be5b9323b43371d150ec66247f9e
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-10-22 14:26:00 +0900

    rust

--- 325/a.rs
+++ 325/a.rs
@@ -1,12 +1,22 @@
 use std::io::{self, BufRead};
 
 fn main() {
+    let (s, _t) = read().unwrap();
+
+    println!("{} san", s);
+}
+
+fn read() -> io::Result<(String, String)> {
     let stdin = io::stdin();
     let mut lines = stdin.lock().lines();
 
-    let input = lines.next().unwrap().unwrap();
-    let words: Vec<&str> = input.split_whitespace().collect();
-    let (s, _t) = (words[0], words[1]);
+    if let Some(Ok(input)) = lines.next() {
+        let words: Vec<String> = input.split_whitespace().map(String::from).collect();
 
-    println!("{} san", s);
+        if words.len() >= 2 {
+            return Ok((words[0].clone(), words[1].clone()));
+        }
+    }
+
+    Err(io::Error::new(io::ErrorKind::InvalidInput, "Insufficient input"))
 }

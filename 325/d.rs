365aae092196512df7f15cd7eccb1bb03be8bbcf
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-10-23 21:16:46 +0900

    wip

---
+++ 325/d.rs
@@ -0,0 +1,44 @@
+use std::collections::BinaryHeap;
+use std::collections::VecDeque;
+use std::io::{self, BufRead};
+
+fn main() {
+    let n = read_single();
+    let items = sorted(read_tuple(n));
+
+    let mut heap = BinaryHeap::new();
+
+    for [a, b] in items {
+        println!("{}, {}", a, b);
+    }
+}
+
+fn sorted(mut items: Vec<[usize; 2]>) -> VecDeque<[usize; 2]> {
+    items.sort();
+    VecDeque::from(items)
+}
+
+fn read_tuple(n: usize) -> Vec<[usize; 2]> {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    let mut result = Vec::new();
+
+    for _ in 0..n {
+        let line = lines.next().unwrap().unwrap();
+        let mut parts = line.split_whitespace();
+        let a = parts.next().unwrap().parse().unwrap();
+        let b = parts.next().unwrap().parse().unwrap();
+
+        result.push([a, b]);
+    }
+
+    result
+}
+
+fn read_single() -> usize {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    lines.next().unwrap().unwrap().parse().unwrap()
+}

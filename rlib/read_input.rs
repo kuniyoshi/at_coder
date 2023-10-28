9c3102c3a8833fc88246c33bebed450fcb96063d
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-10-28 15:04:08 +0900

    format

--- rlib/read_input.rs
+++ rlib/read_input.rs
@@ -1,6 +1,6 @@
+use std::fmt::Debug;
 use std::io::{self, BufRead};
 use std::str::FromStr;
-use std::fmt::Debug;
 
 pub struct ReadInput;
 
@@ -11,11 +11,11 @@ impl ReadInput {
         T::Err: Debug + 'static,
     {
         let mut lines = io::stdin().lock().lines();
-        
+
         if let Some(Ok(line)) = lines.next() {
             return line.parse().map_err(|e| Box::new(e) as Box<dyn Debug>);
         }
-        
+
         Err(Box::new("Failed to read line"))
     }
 
@@ -28,8 +28,16 @@ impl ReadInput {
     {
         let line = Self::read_one::<String>()?;
         let mut parts = line.split_whitespace();
-        let a: T = parts.next().ok_or(Box::new("Missing first element") as Box<dyn Debug>)?.parse().map_err(|e| Box::new(e) as Box<dyn Debug>)?;
-        let b: U = parts.next().ok_or(Box::new("Missing second element") as Box<dyn Debug>)?.parse().map_err(|e| Box::new(e) as Box<dyn Debug>)?;
+        let a: T = parts
+            .next()
+            .ok_or(Box::new("Missing first element") as Box<dyn Debug>)?
+            .parse()
+            .map_err(|e| Box::new(e) as Box<dyn Debug>)?;
+        let b: U = parts
+            .next()
+            .ok_or(Box::new("Missing second element") as Box<dyn Debug>)?
+            .parse()
+            .map_err(|e| Box::new(e) as Box<dyn Debug>)?;
         Ok((a, b))
     }
 
@@ -44,23 +52,36 @@ impl ReadInput {
     {
         let line = Self::read_one::<String>()?;
         let mut parts = line.split_whitespace();
-        let a: T = parts.next().ok_or(Box::new("Missing first element") as Box<dyn Debug>)?.parse().map_err(|e| Box::new(e) as Box<dyn Debug>)?;
-        let b: U = parts.next().ok_or(Box::new("Missing second element") as Box<dyn Debug>)?.parse().map_err(|e| Box::new(e) as Box<dyn Debug>)?;
-        let c: V = parts.next().ok_or(Box::new("Missing third element") as Box<dyn Debug>)?.parse().map_err(|e| Box::new(e) as Box<dyn Debug>)?;
+        let a: T = parts
+            .next()
+            .ok_or(Box::new("Missing first element") as Box<dyn Debug>)?
+            .parse()
+            .map_err(|e| Box::new(e) as Box<dyn Debug>)?;
+        let b: U = parts
+            .next()
+            .ok_or(Box::new("Missing second element") as Box<dyn Debug>)?
+            .parse()
+            .map_err(|e| Box::new(e) as Box<dyn Debug>)?;
+        let c: V = parts
+            .next()
+            .ok_or(Box::new("Missing third element") as Box<dyn Debug>)?
+            .parse()
+            .map_err(|e| Box::new(e) as Box<dyn Debug>)?;
         Ok((a, b, c))
     }
 
     pub fn read_tuples<T: FromStr>(n: usize) -> Result<Vec<(T, T)>, Box<dyn Debug>>
-    where T::Err: Debug + 'static,
+    where
+        T::Err: Debug + 'static,
     {
         let stdin = io::stdin();
         let mut lines = stdin.lock().lines();
         let mut result = Vec::new();
-    
+
         for i in 0..n {
             if let Some(Ok(line)) = lines.next() {
                 let parts: Result<Vec<T>, _> = line.split_whitespace().map(|s| s.parse()).collect();
-                
+
                 match parts {
                     Ok(mut parts) => {
                         if parts.len() == 2 {
@@ -68,30 +89,34 @@ impl ReadInput {
                             let a = parts.pop().unwrap();
                             result.push((a, b));
                         } else {
-                            return Err(Box::new(format!("Error: Line {} does not contain exactly two elements.", i + 1)) as Box<dyn Debug>);
+                            return Err(Box::new(format!(
+                                "Error: Line {} does not contain exactly two elements.",
+                                i + 1
+                            )) as Box<dyn Debug>);
                         }
-                    },
+                    }
                     Err(e) => return Err(Box::new(e)),
                 }
             } else {
                 return Err(Box::new("Failed to read line from stdin.") as Box<dyn Debug>);
             }
         }
-    
+
         Ok(result)
     }
 
     pub fn read_triples<T: FromStr>(n: usize) -> Result<Vec<(T, T, T)>, Box<dyn Debug>>
-    where T::Err: Debug + 'static,
+    where
+        T::Err: Debug + 'static,
     {
         let stdin = io::stdin();
         let mut lines = stdin.lock().lines();
         let mut result = Vec::new();
-    
+
         for i in 0..n {
             if let Some(Ok(line)) = lines.next() {
                 let parts: Result<Vec<T>, _> = line.split_whitespace().map(|s| s.parse()).collect();
-                
+
                 match parts {
                     Ok(mut parts) => {
                         if parts.len() == 3 {
@@ -100,16 +125,19 @@ impl ReadInput {
                             let a = parts.pop().unwrap();
                             result.push((a, b, c));
                         } else {
-                            return Err(Box::new(format!("Error: Line {} does not contain exactly two elements.", i + 1)) as Box<dyn Debug>);
+                            return Err(Box::new(format!(
+                                "Error: Line {} does not contain exactly two elements.",
+                                i + 1
+                            )) as Box<dyn Debug>);
                         }
-                    },
+                    }
                     Err(e) => return Err(Box::new(e)),
                 }
             } else {
                 return Err(Box::new("Failed to read line from stdin.") as Box<dyn Debug>);
             }
         }
-    
+
         Ok(result)
     }
 
@@ -120,11 +148,10 @@ impl ReadInput {
     {
         let stdin = io::stdin();
         let mut lines = stdin.lock().lines();
-        
+
         if let Some(Ok(line)) = lines.next() {
-            let parsed_values: Result<Vec<T>, _> = line.split_whitespace()
-                .map(|s| s.parse())
-                .collect();
+            let parsed_values: Result<Vec<T>, _> =
+                line.split_whitespace().map(|s| s.parse()).collect();
 
             return match parsed_values {
                 Ok(values) => Ok(values),
@@ -142,8 +169,7 @@ impl ReadInput {
         for _ in 0..n {
             if let Some(Ok(line)) = lines.next() {
                 result.push(line.chars().collect());
-            }
-            else {
+            } else {
                 panic!("Could not read line");
             }
         }
@@ -152,7 +178,8 @@ impl ReadInput {
     }
 
     pub fn read_matrix<T: FromStr>(h: usize, w: usize) -> Result<Vec<Vec<T>>, Box<dyn Debug>>
-    where T::Err: Debug + 'static,
+    where
+        T::Err: Debug + 'static,
     {
         let stdin = io::stdin();
         let mut lines = stdin.lock().lines();
@@ -166,9 +193,11 @@ impl ReadInput {
                         if row.len() == w {
                             matrix.push(row);
                         } else {
-                            return Err(Box::new("Row length does not match width") as Box<dyn Debug>);
+                            return Err(
+                                Box::new("Row length does not match width") as Box<dyn Debug>
+                            );
                         }
-                    },
+                    }
                     Err(e) => return Err(Box::new(e) as Box<dyn Debug>),
                 }
             } else {
@@ -184,7 +213,13 @@ fn main() {
     println!("read matrix 2x3");
     let g: Vec<Vec<usize>> = ReadInput::read_matrix(2, 3).unwrap();
     for row in g {
-        println!("{}", row.iter().map(|n| n.to_string()).collect::<Vec<String>>().join(", "));
+        println!(
+            "{}",
+            row.iter()
+                .map(|n| n.to_string())
+                .collect::<Vec<String>>()
+                .join(", ")
+        );
     }
 
     println!("read triples x 3");
@@ -207,7 +242,13 @@ fn main() {
 
     println!("read Vec<usize>");
     let d: Vec<usize> = ReadInput::read_values().unwrap();
-    println!("{}", d.iter().map(|&x| x.to_string()).collect::<Vec<String>>().join(", "));
+    println!(
+        "{}",
+        d.iter()
+            .map(|&x| x.to_string())
+            .collect::<Vec<String>>()
+            .join(", ")
+    );
 
     println!("read (usize, usize, usize)");
     let (c1, c2, c3): (usize, usize, usize) = ReadInput::read_three().unwrap();

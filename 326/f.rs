04f9e45a570c39b8434414bfe98600013cbac0fb
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-11-03 21:10:26 +0900

    326.f: begin

---
+++ 326/f.rs
@@ -0,0 +1,38 @@
+use fmt::Debug;
+use std::fmt;
+use std::io::{self, BufRead};
+use std::str;
+use std::collections::HashMap;
+
+fn main() {
+    let mut lines = io::stdin().lock().lines();
+    let (n, x, y): (usize, i32, i32) = read_three(&mut lines);
+    let a: Vec<i32> = read_list(&mut lines);
+}
+
+fn read_three<A: str::FromStr, B: str::FromStr, C: str::FromStr>(
+    lines: &mut io::Lines<io::StdinLock>,
+) -> (A, B, C)
+where
+    A::Err: Debug + 'static,
+    B::Err: Debug + 'static,
+    C::Err: Debug + 'static,
+{
+    let line = lines.next().unwrap().unwrap();
+    let mut parts = line.split_whitespace();
+    let a: A = parts.next().unwrap().parse().unwrap();
+    let b: B = parts.next().unwrap().parse().unwrap();
+    let c: C = parts.next().unwrap().parse().unwrap();
+    (a, b, c)
+}
+
+fn read_list<A: str::FromStr>(lines: &mut io::Lines<io::StdinLock>) -> Vec<A>
+where
+    A::Err: Debug + 'static,
+{
+    let line = lines.next().unwrap().unwrap();
+    line.split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+

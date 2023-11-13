2912785d4cc62172cd3856cb234704ad0a65ea6d
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-11-13 20:55:41 +0900

    327.e: wip

--- 327/e.rs
+++ 327/e.rs
@@ -1,19 +1,63 @@
 use fmt::Debug;
-use std::collections::HashMap;
 use std::fmt;
 use std::io::{self, BufRead};
 use std::str;
 
 fn main() {
     let mut lines = io::stdin().lock().lines();
+    let n: usize = read_one(&mut lines);
+    let p: Vec<usize> = read_list(&mut lines);
+
+    let mut dp: Vec<Vec<Option<f64>>> = vec!(vec!(None; n + 1); n + 1);
+    dp[0][0] = Some(0_f64);
+
+    for i in 1..=n {
+        for j in 0..=n {
+            dp[i][j] = max(dp[i - 1][j], dp[i][j]);
+            if j + 1 < n {
+                dp[i][j + 1] = max2(dp[i - 1][j], p[i - 1] as f64, dp[i][j + 1]);
+            }
+        }
+    }
+
+    let mut max: f64 = std::f64::MIN;
+
+    for k in 1..=n {
+        if let Some(dp_val) = dp[n][k] {
+            max = fmax(
+                max,
+                dp_val / denominator(k) - 1200_f64.sqrt() / k as f64
+            );
+        } else {
+            max = fmax(max, 1200_f64.sqrt() / k as f64);
+        }
+    }
+
+    println!("{}", max);
 }
 
-fn yes_no(is_yes: bool) -> String {
-    if is_yes {
-        "Yes".to_string()
-    } else {
-        "No".to_string()
+fn denominator(k: usize) -> f64 {
+    (1..=k).map(|i| 0.9_f64.powf((k - i) as f64)).sum()
+}
+
+fn max2(a: Option<f64>, b: f64, c: Option<f64>) -> Option<f64> {
+    match (a, c) {
+        (Some(a_val), Some(_)) => max(Some(a_val * 0.9 + b), c),
+        (Some(a_val), None) => Some(a_val * 0.9 + b),
+        (_, _) => c,
     }
+}
+
+fn max(a: Option<f64>, b: Option<f64>) -> Option<f64> {
+    match (a, b) {
+        (Some(a_val), Some(b_val)) => Some(fmax(a_val, b_val)),
+        (None, _) => b,
+        (_, None) => a,
+    }
+}
+
+fn fmax(a: f64, b: f64) -> f64 {
+    if a > b { a } else { b }
 }
 
 fn read_list<A: str::FromStr>(lines: &mut io::Lines<io::StdinLock>) -> Vec<A>
@@ -24,52 +68,6 @@ where
     line.split_whitespace()
         .map(|s| s.parse().unwrap())
         .collect()
-}
-
-fn read_four<A: str::FromStr, B: str::FromStr, C: str::FromStr, D: str::FromStr>(
-    lines: &mut io::Lines<io::StdinLock>,
-) -> (A, B, C, D)
-where
-    A::Err: Debug + 'static,
-    B::Err: Debug + 'static,
-    C::Err: Debug + 'static,
-    D::Err: Debug + 'static,
-{
-    let line = lines.next().unwrap().unwrap();
-    let mut parts = line.split_whitespace();
-    let a: A = parts.next().unwrap().parse().unwrap();
-    let b: B = parts.next().unwrap().parse().unwrap();
-    let c: C = parts.next().unwrap().parse().unwrap();
-    let d: D = parts.next().unwrap().parse().unwrap();
-    (a, b, c, d)
-}
-
-fn read_three<A: str::FromStr, B: str::FromStr, C: str::FromStr>(
-    lines: &mut io::Lines<io::StdinLock>,
-) -> (A, B, C)
-where
-    A::Err: Debug + 'static,
-    B::Err: Debug + 'static,
-    C::Err: Debug + 'static,
-{
-    let line = lines.next().unwrap().unwrap();
-    let mut parts = line.split_whitespace();
-    let a: A = parts.next().unwrap().parse().unwrap();
-    let b: B = parts.next().unwrap().parse().unwrap();
-    let c: C = parts.next().unwrap().parse().unwrap();
-    (a, b, c)
-}
-
-fn read_two<A: str::FromStr, B: str::FromStr>(lines: &mut io::Lines<io::StdinLock>) -> (A, B)
-where
-    A::Err: Debug + 'static,
-    B::Err: Debug + 'static,
-{
-    let line = lines.next().unwrap().unwrap();
-    let mut parts = line.split_whitespace();
-    let a: A = parts.next().unwrap().parse().unwrap();
-    let b: B = parts.next().unwrap().parse().unwrap();
-    (a, b)
 }
 
 fn read_one<A: str::FromStr>(lines: &mut io::Lines<io::StdinLock>) -> A

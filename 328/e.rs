b53efce83cf9e30941498bb8ea0ec3a4a7ba2bc7
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-11-14 20:37:36 +0900

    328.e: tidy up

--- 328/e.rs
+++ 328/e.rs
@@ -1,6 +1,5 @@
 use fmt::Debug;
 use std::cmp;
-use std::collections::HashMap;
 use std::fmt;
 use std::io::{self, BufRead};
 use std::str;
@@ -72,42 +71,6 @@ fn main() {
     println!("{}", min);
 }
 
-fn yes_no(is_yes: bool) -> &'static str {
-    if is_yes {
-        "Yes"
-    } else {
-        "No"
-    }
-}
-
-fn read_list<A: str::FromStr>(lines: &mut io::Lines<io::StdinLock>) -> Vec<A>
-where
-    A::Err: Debug + 'static,
-{
-    let line = lines.next().unwrap().unwrap();
-    line.split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect()
-}
-
-fn read_four<A: str::FromStr, B: str::FromStr, C: str::FromStr, D: str::FromStr>(
-    lines: &mut io::Lines<io::StdinLock>,
-) -> (A, B, C, D)
-where
-    A::Err: Debug + 'static,
-    B::Err: Debug + 'static,
-    C::Err: Debug + 'static,
-    D::Err: Debug + 'static,
-{
-    let line = lines.next().unwrap().unwrap();
-    let mut parts = line.split_whitespace();
-    let a: A = parts.next().unwrap().parse().unwrap();
-    let b: B = parts.next().unwrap().parse().unwrap();
-    let c: C = parts.next().unwrap().parse().unwrap();
-    let d: D = parts.next().unwrap().parse().unwrap();
-    (a, b, c, d)
-}
-
 fn read_three<A: str::FromStr, B: str::FromStr, C: str::FromStr>(
     lines: &mut io::Lines<io::StdinLock>,
 ) -> (A, B, C)
@@ -122,23 +85,4 @@ where
     let b: B = parts.next().unwrap().parse().unwrap();
     let c: C = parts.next().unwrap().parse().unwrap();
     (a, b, c)
-}
-
-fn read_two<A: str::FromStr, B: str::FromStr>(lines: &mut io::Lines<io::StdinLock>) -> (A, B)
-where
-    A::Err: Debug + 'static,
-    B::Err: Debug + 'static,
-{
-    let line = lines.next().unwrap().unwrap();
-    let mut parts = line.split_whitespace();
-    let a: A = parts.next().unwrap().parse().unwrap();
-    let b: B = parts.next().unwrap().parse().unwrap();
-    (a, b)
-}
-
-fn read_one<A: str::FromStr>(lines: &mut io::Lines<io::StdinLock>) -> A
-where
-    A::Err: Debug + 'static,
-{
-    lines.next().unwrap().unwrap().parse().unwrap()
 }

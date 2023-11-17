5d2c2277e68f98dd79362d4238efb47742209827
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-11-17 20:11:11 +0900

    328.e: wip tle

--- 328/e.rs
+++ 328/e.rs
@@ -4,6 +4,71 @@ use std::fmt;
 use std::io::{self, BufRead};
 use std::str;
 
+struct P {
+    n: usize,
+    m: usize,
+    k: usize,
+    edges: Vec<(usize, usize, usize)>,
+}
+
+fn bits(mut bits: usize) -> usize {
+    let mut count: usize = 0;
+
+    while bits > 0 {
+        if bits & 1 > 0 {
+            count += 1;
+        }
+
+        bits >>= 1;
+    }
+
+    count
+}
+
+fn cost(used: usize, p: &P) -> usize {
+    let mut cost: usize = 0;
+
+    for i in 0..p.m {
+        if (used & 1 << i) > 0 {
+            cost += p.edges[i].2 % p.k;
+        }
+    }
+
+    cost % p.k
+}
+
+fn dfs(visited: usize, used: usize, p: &P) -> Option<usize> {
+    if visited == (1 << p.n) - 1 {
+        return if bits(used) == p.n - 1 {
+            Some(cost(used, p))
+        } else {
+            None
+        };
+    }
+
+    let mut min: Option<usize> = None;
+
+    for i in 0..p.m {
+        if (used & 1 << i) > 0 {
+            continue;
+        }
+
+        let new_visited = visited | 1 << p.edges[i].0 | 1 << p.edges[i].1;
+
+        min = min_or(min, dfs(new_visited, used | 1 << i, p));
+    }
+
+    min
+}
+
+fn min_or(a: Option<usize>, b: Option<usize>) -> Option<usize> {
+    match (a, b) {
+        (Some(a_value), Some(b_value)) => Some(cmp::min(a_value, b_value)),
+        (Some(_), _) => a,
+        (_, _) => b,
+    }
+}
+
 fn main() {
     let mut lines = io::stdin().lock().lines();
     let (n, m, k): (usize, usize, usize) = read_three(&mut lines);
@@ -14,42 +79,44 @@ fn main() {
         })
         .collect();
 
-    let mut min = k - 1;
+    println!("{}", dfs(0, 0, &P { n, m, k, edges }).unwrap());
 
-    'LOOP: for flags in 0..(1 << m) {
-        let mut cost = 0;
-        let mut vertexes: usize = 0;
-        let mut marked: usize = 0;
+    // let mut min = k - 1;
 
-        for i in 0..m {
-            if (1 << i) & flags > 0 {
-                cost += edges[i].2;
-                vertexes |= 1 << edges[i].0;
-                vertexes |= 1 << edges[i].1;
-                marked += 1;
-            }
+    // 'LOOP: for flags in 0..(1 << m) {
+    //     let mut cost = 0;
+    //     let mut vertexes: usize = 0;
+    //     let mut marked: usize = 0;
 
-            if marked > n - 1 {
-                continue 'LOOP;
-            }
-        }
+    //     for i in 0..m {
+    //         if (1 << i) & flags > 0 {
+    //             cost += edges[i].2;
+    //             vertexes |= 1 << edges[i].0;
+    //             vertexes |= 1 << edges[i].1;
+    //             marked += 1;
+    //         }
 
-        if marked != n - 1 {
-            continue;
-        }
+    //         if marked > n - 1 {
+    //             continue 'LOOP;
+    //         }
+    //     }
 
-        if vertexes != (1 << n) - 1 {
-            continue;
-        }
+    //     if marked != n - 1 {
+    //         continue;
+    //     }
 
-        cost %= k;
+    //     if vertexes != (1 << n) - 1 {
+    //         continue;
+    //     }
 
-        // println!("flags: {:b}, {}", flags, cost);
+    //     cost %= k;
 
-        min = cmp::min(min, cost);
-    }
+    //     // println!("flags: {:b}, {}", flags, cost);
 
-    println!("{}", min);
+    //     min = cmp::min(min, cost);
+    // }
+
+    // println!("{}", min);
 }
 
 fn read_three<A: str::FromStr, B: str::FromStr, C: str::FromStr>(

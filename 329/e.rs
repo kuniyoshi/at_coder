065c34e8826e712f1078367f686b656a50ab74b1
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-11-27 20:01:28 +0900

    329.e: wip

--- 329/e.rs
+++ 329/e.rs
@@ -1,11 +1,27 @@
 use fmt::Debug;
-use std::collections::HashMap;
 use std::fmt;
 use std::io::{self, BufRead};
 use std::str;
 
 fn main() {
     let mut lines = io::stdin().lock().lines();
+    let (n, m): (usize, usize) = read_two(&mut lines);
+    let s: Vec<char> = lines.next().unwrap().unwrap().chars().collect();
+    let t: Vec<char> = lines.next().unwrap().unwrap().chars().collect();
+
+    let mut passed: usize = n;
+
+    for i in (0..n).rev() {
+        if i > passed {
+            continue;
+        }
+
+        for j in (0..m).rev() {
+            s[i - j]
+        }
+    }
+
+    println!("{}", yes_no(passed == 0));
 }
 
 fn yes_no(is_yes: bool) -> &'static str {
@@ -14,50 +30,6 @@ fn yes_no(is_yes: bool) -> &'static str {
     } else {
         "No"
     }
-}
-
-fn read_list<A: str::FromStr>(lines: &mut io::Lines<io::StdinLock>) -> Vec<A>
-where
-    A::Err: Debug + 'static,
-{
-    let line = lines.next().unwrap().unwrap();
-    line.split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect()
-}
-
-fn read_four<A: str::FromStr, B: str::FromStr, C: str::FromStr, D: str::FromStr>(
-    lines: &mut io::Lines<io::StdinLock>,
-) -> (A, B, C, D)
-where
-    A::Err: Debug + 'static,
-    B::Err: Debug + 'static,
-    C::Err: Debug + 'static,
-    D::Err: Debug + 'static,
-{
-    let line = lines.next().unwrap().unwrap();
-    let mut parts = line.split_whitespace();
-    let a: A = parts.next().unwrap().parse().unwrap();
-    let b: B = parts.next().unwrap().parse().unwrap();
-    let c: C = parts.next().unwrap().parse().unwrap();
-    let d: D = parts.next().unwrap().parse().unwrap();
-    (a, b, c, d)
-}
-
-fn read_three<A: str::FromStr, B: str::FromStr, C: str::FromStr>(
-    lines: &mut io::Lines<io::StdinLock>,
-) -> (A, B, C)
-where
-    A::Err: Debug + 'static,
-    B::Err: Debug + 'static,
-    C::Err: Debug + 'static,
-{
-    let line = lines.next().unwrap().unwrap();
-    let mut parts = line.split_whitespace();
-    let a: A = parts.next().unwrap().parse().unwrap();
-    let b: B = parts.next().unwrap().parse().unwrap();
-    let c: C = parts.next().unwrap().parse().unwrap();
-    (a, b, c)
 }
 
 fn read_two<A: str::FromStr, B: str::FromStr>(lines: &mut io::Lines<io::StdinLock>) -> (A, B)
@@ -70,11 +42,4 @@ where
     let a: A = parts.next().unwrap().parse().unwrap();
     let b: B = parts.next().unwrap().parse().unwrap();
     (a, b)
-}
-
-fn read_one<A: str::FromStr>(lines: &mut io::Lines<io::StdinLock>) -> A
-where
-    A::Err: Debug + 'static,
-{
-    lines.next().unwrap().unwrap().parse().unwrap()
 }

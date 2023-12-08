314a42b25e1c91ea7752c11eb4ff84e2dc0777ef
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-12-08 20:26:54 +0900

    331.c: alt

--- 331/c2.rs
+++ 331/c2.rs
@@ -1,34 +1,40 @@
-use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::io::{self, BufRead};
 
 fn main() {
     let mut lines = io::stdin().lock().lines();
     let n: usize = lines.next().unwrap().unwrap().parse().unwrap();
-    let mut a: Vec<(usize, usize)> = lines
+    let mut a: Vec<usize> = lines
         .next()
         .unwrap()
         .unwrap()
         .split_whitespace()
         .map(|s| s.parse().unwrap())
-        .enumerate()
-        .map(|(i, v)| (v, i))
         .collect();
 
-    a.sort_by(|a, b| match b.0.cmp(&a.0) {
-        Ordering::Equal => b.1.cmp(&a.1),
-        other => other,
-    });
-
-    let mut acc: usize = 0;
-    let mut result: Vec<usize> = vec![0; n];
+    let mut indexes: HashMap<usize, Vec<usize>> = HashMap::new();
 
     for i in 0..n {
-        result[a[i].1] = acc;
-        acc += a[i].0;
+        let entry = indexes.entry(a[i]).or_insert(Vec::new());
+        entry.push(i);
+    }
+
+    a.sort_by(|a, b| b.cmp(&a));
+    a.dedup();
+
+    let mut results: Vec<usize> = vec![0; n];
+    let mut acc: usize = 0;
+
+    for value in &a {
+        let entry = indexes.entry(*value).or_insert(Vec::new());
+        for i in 0..entry.len() {
+            results[entry[i]] = acc;
+        }
+        acc += value * entry.len();
     }
 
     for i in 0..n {
-        print!("{} ", result[i]);
+        print!("{} ", results[i]);
     }
 
     println!("");

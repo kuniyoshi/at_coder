a1ca1e1bdfa2c7c4be4e827665f7314654723d50
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-12-13 20:30:56 +0900

    tessoku a70: ac

--- tessokur/a70.rs
+++ tessokur/a70.rs
@@ -21,21 +21,47 @@ fn main() {
         .collect();
     let switches: Vec<usize> = (0..m)
         .map(|_| {
-            let list: Vec<usize> = lines
-                .next()
-                .unwrap()
-                .unwrap()
+            let line = lines.next().unwrap().unwrap();
+            // eprintln!("{:?}", line);
+            let list: Vec<usize> = line
                 .split_whitespace()
-                .map(|s| s.parse().unwrap())
+                .map(|s| s.parse().unwrap_or_else(|_| panic!("error: {}", s)))
                 .collect();
             bits3(list[0], list[1], list[2])
         })
         .collect();
 
-    eprintln!("{:?}", switches);
+    // eprintln!("{:?}", switches);
 
     let mut dp: Vec<Vec<Option<usize>>> = vec![vec![None; 1 + 1 << n]; m + 1];
     dp[0][bits(&a)] = Some(0);
+
+    for i in 0..m {
+        for j in 0..dp[i].len() {
+            match dp[i][j] {
+                Some(v) => {
+                    dp[i + 1][j] = Some(dp[i + 1][j].map_or_else(|| v, |x| x.min(v)));
+                    dp[i + 1][j ^ switches[i]] =
+                        Some(dp[i + 1][j ^ switches[i]].map_or_else(|| v + 1, |x| x.min(v + 1)));
+                }
+                None => (),
+            };
+        }
+    }
+
+    // for i in 0..dp.len() {
+    //     for j in 0..dp[i].len() {
+    //         match dp[i][j] {
+    //             Some(v) => println!(" {} {} -> {}", i, format!("{:b}", j), v),
+    //             None => (),
+    //         }
+    //     }
+    // }
+
+    match dp[m][(1 << n) - 1] {
+        Some(c) => println!("{}", c),
+        None => println!("{}", -1),
+    };
 }
 
 fn bits3(a: usize, b: usize, c: usize) -> usize {

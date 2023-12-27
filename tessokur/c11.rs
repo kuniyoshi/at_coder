dd4f2284530e7a00613fe6cdb153b017e726c49e
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2023-12-27 15:47:03 +0900

    tessoku c11: wip

--- tessokur/c11.rs
+++ tessokur/c11.rs
@@ -1,5 +1,25 @@
+use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 use std::io::{self, BufRead};
+
+#[derive(PartialEq, Eq, Debug)]
+struct Item {
+    votes: usize,
+    kth: usize,
+    tou: usize,
+}
+
+impl Ord for Item {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.votes.cmp(&other.votes)
+    }
+}
+
+impl PartialOrd for Item {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
 fn main() {
     let mut lines = io::stdin().lock().lines();
@@ -21,30 +41,33 @@ fn main() {
         .map(|s| s.parse().unwrap())
         .collect();
 
-    let mut heap: BinaryHeap<(usize, usize)> = BinaryHeap::new();
+    let mut heap: BinaryHeap<Item> = BinaryHeap::new();
 
     for i in 0..n {
-        for j in 1..=k {
-            heap.push((a[i] * 1_000_000_000 / j, i));
-        }
+        heap.push(Item {
+            votes: a[i] * 1_000_000_000,
+            kth: 1,
+            tou: i,
+        });
     }
 
     let mut seats: Vec<usize> = vec![0; n];
     let mut got: usize = 0;
 
-    while heap.len() > 0 {
+    while got < k && heap.len() > 0 {
         match heap.pop() {
-            Some((_, tou)) => {
-                seats[tou] += 1;
+            Some(item) => {
+                seats[item.tou] += 1;
+                heap.push(Item {
+                    votes: a[item.tou] * 1_000_000_000 / (item.kth + 1),
+                    kth: item.kth + 1,
+                    tou: item.tou,
+                });
             }
             _ => panic!("unknown"),
         }
 
         got += 1;
-
-        if got == k {
-            break;
-        }
     }
 
     for i in 0..n {

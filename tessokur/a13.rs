6bc67e44cbeda87d6104451335dc0df502505fab
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-01-01 17:15:25 +0900

    tessoku a13

--- tessokur/a13.rs
+++ tessokur/a13.rs
@@ -7,77 +7,39 @@ fn main() {
         (list[0], list[1])
     };
     let a: Vec<usize> = lines.next().unwrap().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
+    
+    // 11 12 16 22 27 28 31
+    // 11, 12
+    // 11, 16
+    // 12, 16
+    // 12, 22
+    // 16, 22
+    // 
 
     let mut count: usize = 0;
 
-    for i in 0..n {
-        let min = lower_bound(if a[i] >= k { a[i] - k } else { a[i] }, &a);
-        let max = upper_bound(a[i] + k, &a);
-
-        match (min, max) {
-            (Some(min_index), Some(max_index)) => count += max_index - min_index,
-            _ => panic!("Could not find lower_bound, and upper_bound"),
+    for i in 0..(n-1) {
+        if a[i] + k >= a[n-1] {
+            count += (n - 1) - i;
+            continue;
         }
-    }
 
-    println!("{}", count / 2);
-}
+        let mut ac = i;
+        let mut wa = n - 1;
 
-// value より小さいもののうち、最大の添字を返す
-fn upper_bound(value: usize, a: &Vec<usize>) -> Option<usize> {
-    if a.len() == 0 {
-        return None;
-    }
-
-    if value < a[0] {
-        return None;
-    }
-
-    if a.last().unwrap() <= &value {
-        return Some(a.len() - 1);
-    }
-
-    let mut ac: usize = a.len() - 1;
-    let mut wa: usize = 0;
-
-    while ac - wa > 1 {
-        let wj = (ac + wa) / 2;
-        if a[wj] <= value {
-            ac = wj;
-        } else {
-            wa = wj;
+        while wa - ac > 1 {
+            let wj = (ac + wa) / 2;
+            if a[wj] <= a[i] + k {
+                ac = wj;
+            }
+            else {
+                wa = wj;
+            }
         }
+
+        // println!("{} {} {} {}", i, a[i], ac, a[ac]);
+        count += ac - i;
     }
 
-    Some(ac)
-}
-
-// value 以上であるもののうち、最小の添え字を返す
-fn lower_bound(value: usize, a: &Vec<usize>) -> Option<usize> {
-    if a.len() == 0 {
-        return None;
-    }
-
-    if &value > a.last().unwrap() {
-        return None;
-    }
-
-    if a[0] >= value {
-        return Some(0);
-    }
-
-    let mut ac: usize = a.len() - 1;
-    let mut wa: usize = 0;
-
-    while ac - wa > 1 {
-        let wj = (ac + wa) / 2;
-        if a[wj] >= value {
-            ac = wj;
-        }
-        else {
-            wa = wj;
-        }
-    }
-
-    Some(ac)
+    println!("{}", count);
 }

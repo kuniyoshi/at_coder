99fa27111f84359c70ef9a6b6bb1720373c48486
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-01-22 20:45:11 +0900

    tessoku a39: wip

--- tessokur/a39.rs
+++ tessokur/a39.rs
@@ -1,30 +1,45 @@
 use std::io::{self, BufRead};
+use std::collections::BinaryHeap;
 
 fn main() {
     let mut lines = io::stdin().lock().lines();
     let n: usize = lines.next().unwrap().unwrap().parse().unwrap();
-    let lr: Vec<(usize, usize)> = (0..n).map(|_| {
+    let mut lr: Vec<(usize, usize)> = (0..n).map(|_| {
         let list: Vec<usize> = lines.next().unwrap().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
         (list[0], list[1])
     }).collect();
 
-    let mut events: Vec<(usize, usize)> = Vec::new();
-
-    let mut total: usize = 0;
-
-    for i in 0..lr.len() {
-        events.push((lr[i].0, i));
-        events.push((lr[i].1, i));
-    }
-
-    events.sort();
+    lr.sort_by(|a, b| a.1.cmp(&b.1));
 
     #[cfg(debug_assertions)]
-    eprintln!("{:?}", events);
+    eprintln!("{:?}", lr);
 
-    let mut did_begins: Vec<bool> = vec![false, n];
+    let mut queue: BinaryHeap<(usize, usize)> = BinaryHeap::new();
 
-    
+    let mut total: usize = 0;
+    let mut last: usize = 0;
+
+    for i in 0..lr.len() {
+        while let Some(end) = queue.peek() {
+            if end.0 < lr[i].0 || end.0 < last {
+                queue.pop();
+                continue;
+            }
+
+            break;
+        }
+
+        match queue.peek() {
+            Some(end) if lr[end.1].0 >= last => {
+                last = lr[end.1].0;
+                total += 1;
+                queue.pop();
+            },
+            _ => (),
+        };
+
+        queue.push((lr[i].1, i));
+    }
 
     println!("{}", total);
 }

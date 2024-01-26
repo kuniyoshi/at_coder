a12ae76c7add96689428672fc7c45cfa3254a50c
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-01-26 20:18:05 +0900

    336.b: alt

--- 336/b.rs
+++ 336/b.rs
@@ -4,20 +4,5 @@ fn main() {
     let mut lines = io::stdin().lock().lines();
     let n: usize = lines.next().unwrap().unwrap().parse().unwrap();
 
-    println!("{}", ctz(n));
-}
-
-fn ctz(mut value: usize) -> usize {
-    let mut count = 0;
-
-    while value > 0 {
-        if value % 2 == 1 {
-            break;
-        }
-
-        count += 1;
-        value >>= 1;
-    }
-
-    count
+    println!("{}", n.trailing_zeros());
 }

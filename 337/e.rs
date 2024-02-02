984a055735a8fcd6fe8fc7e1ddcb8e9f51eeddef
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-02-02 20:26:54 +0900

    337.e: wip

--- 337/e.rs
+++ 337/e.rs
@@ -4,17 +4,22 @@ use std::io::Write;
 fn main() {
     let mut lines = io::stdin().lock().lines();
     let n: usize = lines.next().unwrap().unwrap().parse().unwrap();
-    println!("{}", n);
-    for i in 0..n {
+    println!("{}", n - 1);
+    for i in 0..(n -1) {
         println!("1 {}", i + 1);
     }
     std::io::stdout().flush().unwrap();
     let crash: String = lines.next().unwrap().unwrap();
-    let pos = crash.chars().position(|c| c == '1').unwrap();
-    println!("{}", pos + 1);
+    let pos = crash.chars().position(|c| c == '1');
+    match pos {
+        None => println!("{}", n),
+        Some(p) => println!("{}", p + 1),
+    }
     std::io::stdout().flush().unwrap();
     // 3
     // 1 1
     // 1 2
     // 1 3
+    // 10
+    // 100
 }

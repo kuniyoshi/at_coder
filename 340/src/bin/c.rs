0c21a4cf448d2c3e291e2b3a0f0d0b9bde5793e6
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-02-26 20:38:40 +0900

    340.c: clean up

--- 340/src/bin/c.rs
+++ 340/src/bin/c.rs
@@ -1,48 +1,51 @@
 use std::io::{self, BufRead};
-
-static mut MEMO: Option<std::collections::HashMap<usize, usize>> = None;
+use std::collections::HashMap;
 
 fn main() {
     let mut lines = io::stdin().lock().lines();
     let n: usize = lines.next().unwrap().unwrap().parse().unwrap();
 
-    unsafe {
-        MEMO = Some(std::collections::HashMap::new());
-    }
+    let mut r = Recurse::new();
 
-    println!("{}", r(n));
+    println!("{}", r.r(n));
 }
 
-fn r(n: usize) -> usize {
-    unsafe {
-        let memo = MEMO.as_mut().unwrap();
+struct Recurse {
+    memo: HashMap<usize, usize>,
+}
 
-        if let Some(&result) = memo.get(&n) {
+impl Recurse {
+    pub fn new() -> Self {
+        Recurse { memo: HashMap::new() }
+    }
+
+    pub fn r(self: &mut Self, n: usize) -> usize {
+        if let Some(&result) = self.memo.get(&n) {
             return result;
         }
 
-        let l = lbin(n);
-        let u = ubin(n);
+        let l = self.lbin(n);
+        let u = self.ubin(n);
 
         let result = if l > 1 && u > 1 {
-            n + r(l) + r(u)
+            n + self.r(l) + self.r(u)
         } else if l > 1 {
-            n + r(l)
+            n + self.r(l)
         } else if u > 1 {
-            n + r(u)
+            n + self.r(u)
         } else {
             n
         };
 
-        memo.insert(n, result);
+        self.memo.insert(n, result);
         result
     }
-}
 
-fn lbin(n: usize) -> usize {
-    n / 2
-}
+    fn lbin(self: &Self, n: usize) -> usize {
+        n / 2
+    }
 
-fn ubin(n: usize) -> usize {
-    (n + 1) / 2
+    fn ubin(self: &Self, n: usize) -> usize {
+        (n + 1) / 2
+    }
 }

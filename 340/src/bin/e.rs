8d5c79e7757ca1ea041afe9e0454808af404480b
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-03-09 20:26:45 +0900

    340.e: wip

--- 340/src/bin/e.rs
+++ 340/src/bin/e.rs
@@ -6,12 +6,62 @@ fn main() {
         let list: Vec<usize> = lines.next().unwrap().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
         (list[0], list[1])
     };
-    let a: Vec<usize> = lines.next().unwrap().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
+    let a: Vec<i64> = lines.next().unwrap().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
     let b: Vec<usize> = lines.next().unwrap().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
 
-    let mut boxes: Vec<usize> = vec![0; n];
+    let mut tree = FenwickTree::new(n);
 
-    for count in &boxes {
-        println!("{}", count);
+    for i in 0..n {
+        match i {
+            0 => tree.update(i + 1, a[i]),
+            _ => {
+                let before = tree.sum(i);
+                tree.update(i + 1, a[i] - before);
+            }
+        }
+    }
+
+    for i in 0..m {
+        let current = tree.sum(b[i] + 1);
+        tree.update(b[i] + 1, -current);
+        let whole = current / n as i64;
+        tree.update(1, tree.sum(1) + whole);
+        let remain = whole as usize % n;
+        if remain == 0 {
+            continue;
+        }
+        tree.update(b[i] + 2, tree.sum(b[i] + 2) + 1);
+    }
+
+    for i in 1..=n {
+        print!("{} ", tree.sum(i));
+    }
+
+    println!("");
+}
+
+struct FenwickTree {
+    tree: Vec<i64>,
+}
+
+impl FenwickTree {
+    fn new(size: usize) -> Self {
+        FenwickTree { tree: vec![0; size + 1] }
+    }
+
+    fn update(&mut self, mut index: usize, value: i64) {
+        while index < self.tree.len() {
+            self.tree[index] += value;
+            index += index & index.wrapping_neg();
+        }
+    }
+
+    fn sum(&self, mut index: usize) -> i64 {
+        let mut result = 0;
+        while index > 0 {
+            result += self.tree[index];
+            index -= index & index.wrapping_neg();
+        }
+        result
     }
 }

c24017ef96f81a46271d143e7a2b47e8362b12c3
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-03-16 17:43:00 +0900

    340.e: wip

--- 340/src/bin/e.rs
+++ 340/src/bin/e.rs
@@ -2,7 +2,7 @@ use std::io::{self, BufRead};
 
 fn main() {
     let mut lines = io::stdin().lock().lines();
-    let (n, m): (usize, usize) = {
+    let (n, _): (usize, usize) = {
         let list: Vec<usize> = lines
             .next()
             .unwrap()
@@ -33,25 +33,59 @@ fn main() {
         tree.update(i + 1, a[i] - tree.sum(i));
     }
 
-    for i in 0..m {
-        println!("{}", i);
-        let whole = tree.sum(b[i] + 1) / n as i64;
-        tree.clear_acc(b[i] + 1);
+    // data: 1 1 1 1 1
+    // acc:  1 2 3 4 5
+
+    // query: 3
+    // acc: 1 2 0 4 5
+    // acc: 2 2 0 5 6
+    // data: 2 0 -2 5 1
+
+    // query: 5
+    // acc: 4 3 1 6 1
+    // data: 4 -1 -2 5 -5
+
+    // query: 1
+    // acc: 0 4 2 7 2
+    // data: 0 4 -2 5 -5
+
+    for i in &b {
+        let box_number = i + 1;
+
+        #[cfg(debug_assertions)]
+        eprintln!("box_number: {:?}", box_number);
+        
+        let acc = tree.sum(box_number);
+        let whole = acc / n as i64;
+
+        #[cfg(debug_assertions)]
+        eprintln!("acc: {:?}", acc);
+
+        #[cfg(debug_assertions)]
+        eprintln!("whole: {:?}", whole);
+
+        assert!(acc >= 0);
+        assert!(whole >= 0);
+
+        tree.clear_acc(box_number);
         tree.update(1, whole);
-        let remain = whole as usize % n;
+        let remain = acc as usize % n;
+
+        #[cfg(debug_assertions)]
+        eprintln!("remain: {:?}", remain);
+
         if remain == 0 {
             continue;
         }
-        if b[i] + 1 < n {
-            tree.update(b[i + 1 + 1], 1);
+        if box_number + 1 <= n {
+            tree.update(box_number + 1, 1);
         }
-        if b[i] + remain < n {
-            tree.update(b[i] + remain + 1 + 1, -1);
+        if box_number + remain < n {
+            tree.update(box_number + remain, -1);
         }
-        if b[i] + remain >= n {
+        if box_number + remain > n {
             tree.update(1, 1);
-            println!("{} {} {}", b[i], remain, n);
-            tree.update(b[i] + 1 + remain - n, -1);
+            tree.update(box_number + 1 + remain - n, -1);
         }
     }
 

3b0b52f7b56b75bfa10b443ef5a4e40453da8b44
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-03-26 20:56:16 +0900

    334.b: wip

--- 334/src/bin/b.rs
+++ 334/src/bin/b.rs
@@ -2,7 +2,7 @@ use std::io::{self, BufRead};
 
 fn main() {
     let mut lines = io::stdin().lock().lines();
-    let (a, m, l, r): (i64, i64, i64, i64) = {
+    let (m, l, r): (i64, i64, i64) = {
         let list: Vec<i64> = lines
             .next()
             .unwrap()
@@ -10,10 +10,9 @@ fn main() {
             .split_whitespace()
             .map(|s| s.parse().unwrap())
             .collect();
-        (list[0], list[1], list[2] - list[0], list[3] - list[0])
+        (list[1], list[2] - list[0], list[3] - list[0])
     };
 
-    // let right = r - l;
     // 5 3
     // -4 -1 2 5 8 11 14
     // 0 3 6
@@ -21,32 +20,41 @@ fn main() {
     // 7
 
     match (l >= 0, r >= 0) {
-        //  3   9
-        // 0 3 6 9 12
+        // [3, 3]
+        // 0, 3
         (true, true) => {
             let right = r / m;
             let left = l / m;
-            println!(
-                "{}",
-                right - left
-                    + if l == 0 || r == 0 { 1 } else { 0 }
-                    + if left > 0 && left % m == 0 { 1 } else { 0 }
-            );
+            println!("{}", right - left + zero(l, r));
         }
-        // -6 1
+        // [-6, 1]
         // -6 -3 0 3
         (false, true) => {
-            let right = r / m + 1;
+            let right = r / m;
             let left = l / m;
-            println!("{}", right + left.abs() + if r == 0 { 1 } else { 0 });
+            println!("{}", right + left.abs() + zero(l, r));
         }
         // -8 -1
         // -9 -6 -3 0
         (false, false) => {
             let right = r / m;
             let left = l / m;
-            println!("{}", right.abs() - left.abs());
+            println!("{}", left.abs() - right.abs() + zero(l, r));
         }
         (_, _) => panic!("invalid"),
+    }
+}
+
+fn zero(l: i64, r: i64) -> i64 {
+    if l < 0 {
+        if r >= 0 {
+            return 1;
+        } else {
+            return 0;
+        }
+    } else if l == 0 {
+        return 1;
+    } else {
+        return 0;
     }
 }

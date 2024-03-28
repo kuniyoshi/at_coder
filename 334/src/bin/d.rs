f2c1fc08d00acbec22d20f2e6c50de6080ac77d8
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-03-28 17:48:21 +0900

    334.d: ac

--- 334/src/bin/d.rs
+++ 334/src/bin/d.rs
@@ -1,2 +1,41 @@
+use std::io::{self, BufRead};
+
 fn main() {
+    let mut lines = io::stdin().lock().lines();
+    let (n, q): (usize, usize) = {
+        let list: Vec<usize> = lines.next().unwrap().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
+        (list[0], list[1])
+    };
+    let mut r: Vec<usize> = lines.next().unwrap().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
+    let queries: Vec<usize> = (0..q).map(|_| { lines.next().unwrap().unwrap().parse().unwrap() }).collect();
+
+    r.sort();
+
+    let mut acc = vec![0];
+
+    for i in 0..r.len() {
+        acc.push(acc[i] + r[i]);
+    }
+
+    for query in &queries {
+        if query >= acc.last().unwrap() {
+            println!("{}", n);
+            continue;
+        }
+
+        let mut ac = 0;
+        let mut wa = n;
+
+        while wa - ac > 1 {
+            let wj = (ac + wa) / 2;
+
+            if acc[wj] <= *query {
+                ac = wj;
+            } else {
+                wa = wj;
+            }
+        }
+
+        println!("{}", ac);
+    }
 }

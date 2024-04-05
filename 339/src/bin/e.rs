54582968b5fb1c24850fdc6cd19a915f465cc9e4
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-04-05 20:02:52 +0900

    339.e: wip

--- 339/src/bin/e.rs
+++ 339/src/bin/e.rs
@@ -1,2 +1,13 @@
+use std::io::{self, BufRead};
+
 fn main() {
+    let mut lines = io::stdin().lock().lines();
+    let (n, d): (usize, usize) = {
+        let list: Vec<usize> = lines.next().unwrap().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
+        (list[0], list[1])
+    };
+    let a: Vec<usize> = lines.next().unwrap().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
+
+    // DP っぽさはある
+    // 
 }

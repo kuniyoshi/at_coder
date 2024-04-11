2d588ea5ac0eabdbc0e4e651b360ed9efe079827
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-04-11 21:36:04 +0900

    345.b: alt

--- 345/src/bin/b.rs
+++ 345/src/bin/b.rs
@@ -4,25 +4,10 @@ fn main() {
     let mut lines = io::stdin().lock().lines();
     let x: i64 = lines.next().unwrap().unwrap().parse().unwrap();
 
-    let mod10 = x % 10;
-
-    if mod10 == 0 {
-        println!("{}", x / 10);
-        return ();
-    }
-
-    if x >= 0 {
-        println!("{}", (x / 10) + 1);
-    } else {
+    if x < 0 {
         println!("{}", x / 10);
     }
-
-    // TODO: 境界が 5 くらい？
-
-    // if x < 0 {
-    //     println!("{}", x / 10);
-    // }
-    // else {
-    //     println!("{}", (x + 5) / 10);
-    // }
+    else {
+        println!("{}", (x + 9) / 10);
+    }
 }

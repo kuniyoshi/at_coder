9490ce599d0212e1be9d33d478a1c0bc982cd2b5
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-04-13 20:48:41 +0900

    345.d: wip

--- 345/src/bin/d.rs
+++ 345/src/bin/d.rs
@@ -1,5 +1,6 @@
 use std::io::{self, BufRead};
 
+#[derive(Debug)]
 struct R {
     h: usize,
     w: usize,
@@ -8,92 +9,62 @@ struct R {
 fn main() {
     let mut lines = io::stdin().lock().lines();
     let (n, h, w): (usize, usize, usize) = {
-        let list: Vec<usize> = lines.next().unwrap().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
+        let list: Vec<usize> = lines
+            .next()
+            .unwrap()
+            .unwrap()
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
         (list[0], list[1], list[2])
     };
-    let bases: Vec<R> = (0..n).map(|_| {
-        let list: Vec<usize> = lines.next().unwrap().unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
-        R { h: list[0], w: list[1] }
-    }).collect();
-
-    let mut rectangles_sets = Vec::new();
-
-    for i in 0..(2^n) {
-        let mut set = Vec::new();
-        for j in 0..n {
-            if ((1 << j) & i) == 1 {
-                set.push(R { h: bases[j].w, w: bases[j].h });
+    let rectangles: Vec<R> = (0..n)
+        .map(|_| {
+            let list: Vec<usize> = lines
+                .next()
+                .unwrap()
+                .unwrap()
+                .split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect();
+            R {
+                h: list[0],
+                w: list[1],
             }
-            else {
-                set.push(R { h: bases[j].h, w: bases[j].w });
-            }
-        }
-        rectangles_sets.push(set);
-    }
+        })
+        .collect();
 
-    for set in &rectangles_sets { // 2^7
-        let mut order = Vec::new();
-        if test(&mut order, set, R { h, w }) { // 7! * h * w
-            println!("Yes");
-            return;
-        }
-    }
+    let mut used = vec![false; n];
+    let mut map = vec![vec![false; w]; h];
 
-    println!("No");
+    if test(&mut used, false, &rectangles, &mut map, R { h, w }) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
 
-fn test(order: &mut Vec<usize>, set: &Vec<R>, map: R) -> bool {
-    if order.len() == set.len() {
-        let mut using = Vec::new();
-        for i in order {
-            using.push(&set[*i]);
+fn cursor(map: &Vec<Vec<bool>>, size: R) -> Option<R> {
+    for i in 0..size.h {
+        for j in 0..size.w {
+            if !map[i][j] {
+                return Some(R { h: i, w: j });
+            }
         }
-        return t(&using, map);
     }
-
-    for i in 0..set.len() {
-        if order.contains(&i) {
-            continue;
-        }
-
-        order.push(i);
-
-        if test(order, set, R { h: map.h, w: map.w }) {
-            return true;
-        }
-
-        order.remove(order.len() - 1);
-    }
-
-    false
+    None
 }
 
-fn t(using: &Vec<&R>, map: R) -> bool {
-    let mut cells = vec![vec![false; map.w]; map.h];
-    let mut cursor = R { h: 0, w: 0 };
-
-    for r in using {
-        if cursor.w + r.w >= map.w || cursor.h + r.h >= map.h {
-            return false;
-        }
-
-        for i in 0..r.h {
-            for j in 0..r.w {
-                if cursor.h + i < map.h && cursor.w + j < map.w {
-                    cells[cursor.h + i][cursor.w + j] = true;
+fn test(used: &mut Vec<bool>, is_angle_horizontal: bool, rectangles: &Vec<R>, map: &mut Vec<Vec<bool>>, size: R) -> bool {
+    match cursor(map, size) {
+        None => true,
+        Some(c) => {
+            for i in 0..used.len() {
+                if !used[i] {
+                    for i in 0..
                 }
             }
+            false
         }
-        if cursor.w + r.w + 1 < map.w {
-            cursor = R { h: cursor.h, w: cursor.w + r.w + 1 };
-            continue;
-        }
-        if cursor.h + r.h + 1 < map.h {
-            cursor = R { h: cursor.h + r.h + 1, w: cursor.w };
-            continue;
-        }
-        break;
     }
-    
-    cells[map.h - 1][map.w - 1]
 }

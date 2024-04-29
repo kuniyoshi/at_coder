43075c4100036492d02772a1279e1c6c2cacbb15
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-04-29 20:42:14 +0900

    training: 342c

---
+++ training/src/bin/342c.rs
@@ -0,0 +1,28 @@
+fn main() {
+    proconio::input! {
+        n: usize,
+        s: proconio::marker::Chars,
+        q: usize,
+        queries: [(char, char); q],
+    }
+
+    #[cfg(debug_assertions)]
+    eprintln!("{:?}", queries);
+
+    let mut chars: Vec<_> = ('a'..='z').collect();
+    #[cfg(debug_assertions)]
+    eprintln!("{:?}", chars);
+
+    for (from, to) in queries {
+        for i in 0..chars.len() {
+            if chars[i] == from {
+                chars[i] = to;
+            }
+        }
+    }
+
+    for i in 0..s.len() {
+        print!("{}", chars[(s[i] as usize) - ('a' as usize)]);
+    }
+    println!("");
+}

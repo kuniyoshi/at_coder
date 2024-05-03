d7b65e8a248cef434acf171a3e3275a4d65b6156
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-05-03 20:42:10 +0900

    training: 233c

--- training/src/bin/233c.rs
+++ training/src/bin/233c.rs
@@ -16,4 +16,22 @@ fn main() {
 
     #[cfg(debug_assertions)]
     eprintln!("{:?}", l);
+
+    println!("{}", dfs(x, 0, &l));
+}
+
+fn dfs(x: usize, i: usize, l: &Vec<Vec<usize>>) -> usize {
+    if i == l.len() {
+        return if x == 1 { 1 } else { 0 };
+    }
+
+    let mut total = 0;
+
+    for j in 0..l[i].len() {
+        if x >= l[i][j] && x % l[i][j] == 0 {
+            total += dfs(x / l[i][j], i + 1, l);
+        }
+    }
+
+    total
 }

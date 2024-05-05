13c46bfb3760bb7c8eb3f5903333b3b8ad4a744f
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-05-05 15:36:43 +0900

    350.c

--- 350/src/bin/c.rs
+++ 350/src/bin/c.rs
@@ -23,27 +23,14 @@ fn main() {
 
     let mut swaps = Vec::new();
 
-    // v: C A B
-    // i: 1 2 0
-    for target in 0..n {
-        // C と A とを swap したい
-        // C は index: target にある
-        // A は index: indexes[v[target]] にある
-
-        let ti = indexes[v[target]];
-        let vt = v[target];
-        v.swap(target, indexes[ti]);
-
-        // 添え字を更新したい
-        // 1 2 0 を 0 2 1 にしたい
-        // indexes[target] と indexes[v[target]] とを交換したい (置き換わってるのに注意)
-        // index == target のところと target とを交換したい
-        // indexes[v[target]]
-        indexes.swap(target, vt);
-
-        if vt != target {
-            swaps.push((target + 1, vt + 1));
+    for t in 0..n {
+        if t != indexes[t] {
+            swaps.push((t + 1, indexes[t] + 1));
         }
+
+        let s = v[t];
+        v.swap(t, indexes[t]);
+        indexes.swap(t, s);
     }
 
     println!("{}", swaps.len());
@@ -54,4 +41,7 @@ fn main() {
 
     #[cfg(debug_assertions)]
     eprintln!("{:?}", v);
+
+    #[cfg(debug_assertions)]
+    eprintln!("{:?}", indexes);
 }

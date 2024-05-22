57947bab10b0e2eede7a2a54324b95e0429200a3
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-05-22 20:46:36 +0900

    training: 257c

--- training/src/bin/257c.rs
+++ training/src/bin/257c.rs
@@ -9,15 +9,24 @@ impl Index {
     }
 
     fn new() -> Self {
-        Self { c_passes: 0, a_passes: 0 }
+        Self {
+            c_passes: 0,
+            a_passes: 0,
+        }
     }
 
     fn inc_c(self: &Self) -> Self {
-        Self { c_passes: self.c_passes + 1, a_passes: self.a_passes }
+        Self {
+            c_passes: self.c_passes + 1,
+            a_passes: self.a_passes,
+        }
     }
 
     fn inc_a(self: &Self) -> Self {
-        Self { c_passes: self.c_passes, a_passes: self.a_passes + 1 }
+        Self {
+            c_passes: self.c_passes,
+            a_passes: self.a_passes + 1,
+        }
     }
 
     fn select(self: &Self, children: &Vec<usize>, adults: &Vec<usize>) -> bool {
@@ -30,6 +39,10 @@ impl Index {
         if self.a_passes >= adults.len() {
             return false;
         }
+        if adults[self.a_passes] < children[self.c_passes] {
+            return true;
+        }
+        false
     }
 }
 
@@ -39,12 +52,6 @@ fn main() {
         s: proconio::marker::Chars,
         w: [usize; n],
     };
-
-    #[cfg(debug_assertions)]
-    eprintln!("{:?}", s);
-
-    #[cfg(debug_assertions)]
-    eprintln!("{:?}", w);
 
     let mut children = Vec::new();
     let mut adults = Vec::new();
@@ -63,21 +70,58 @@ fn main() {
     let mut cursor = Index::new();
     let mut wrongs = 0;
 
-    let min = children.first().unwrap_or(&0).min(adults.first().unwrap_or(&0));
+    let min = children.iter().chain(adults.iter()).min().copied().unwrap();
+
+    #[cfg(debug_assertions)]
+    eprintln!("{:?}", children);
+
+    #[cfg(debug_assertions)]
+    eprintln!("{:?}", adults);
+
+    #[cfg(debug_assertions)]
+    eprintln!("min: {:?}", min);
 
     for child in &children {
-        if child >= min {
+        if child >= &min {
             wrongs += 1;
         }
     }
 
     for adult in &adults {
-        if adult < min {
+        if adult < &min {
             wrongs += 1;
         }
     }
 
-    while cursor.passes() < n {
+    let mut result = n - wrongs;
 
+    while cursor.passes() < n {
+        #[cfg(debug_assertions)]
+        eprintln!("wrongs: {:?}", wrongs);
+
+        let value = match cursor.select(&children, &adults) {
+            true => adults[cursor.a_passes],
+            false => children[cursor.c_passes],
+        };
+
+        for i in cursor.a_passes..adults.len() {
+            if adults[i] != value {
+                break;
+            }
+            cursor = cursor.inc_a();
+            wrongs += 1;
+        }
+
+        for i in cursor.c_passes..children.len() {
+            if children[i] != value {
+                break;
+            }
+            cursor = cursor.inc_c();
+            wrongs -= 1;
+        }
+
+        result = result.max(n - wrongs)
     }
+
+    println!("{}", result);
 }

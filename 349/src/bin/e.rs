2b8a2a6ecb8df9b03b9ba117f9d36ac0fb31ed0b
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-05-23 20:45:57 +0900

    349.e: wip

--- 349/src/bin/e.rs
+++ 349/src/bin/e.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 fn main() {
     proconio::input! {
         a: [[i64; 3]; 3],
@@ -9,74 +7,66 @@ fn main() {
     eprintln!("{:?}", a);
 
     let mut board = vec![vec![None::<bool>; 3]; 3];
-    let mut cache = HashMap::new();
 
-    let is_takahashi = dfs(true, &mut board, &a, &mut cache, 0, 0);
+    let is_takahashi = dfs(&mut board, &a);
     println!("{}", if is_takahashi { "Takahashi" } else { "Aoki" });
 }
 
-fn hash(board: &Vec<Vec<Option<bool>>>) -> usize {
-    let mut value = 0;
+fn is_win(board: &Vec<Vec<Option<bool>>>, a: &Vec<Vec<i64>>) -> Option<bool> {
+    if board.iter().flat_map(|v| v.iter()).any(|v| v.is_none()) {
+        return None;
+    }
+
+    if board.iter().any(|cols| cols.iter().all(|v| v.unwrap())) {
+        return Some(true);
+    }
+
+    if (0..3).any(|j| (0..3).all(|i| board[i][j].unwrap())) {
+        return Some(true);
+    }
+
+    if (0..3).all(|i| board[i][i].unwrap()) {
+        return Some(true);
+    }
+
+    if (0..3).all(|i| board[i][2 - i].unwrap()) {
+        return Some(true);
+    }
+
+    let mut me = 0;
+    let mut other = 0;
 
     for i in 0..3 {
         for j in 0..3 {
-            match board[i][j] {
-                Some(true) => {
-                    // 0
-                    value += 9 * i + 3 * j + 0;
-                }
-                Some(false) => {
-                    // 1
-                    value += 9 * i + 3 * j + 1;
-                }
-                None => {
-                    // 2
-                    value += 9 * i + 3 * j + 2;
-                }
+            if board[i][j].unwrap() {
+                me += a[i][j];
+            } else {
+                other += a[i][j];
             }
         }
     }
 
-    value
+    Some(me > other)
 }
 
-fn is_win(board: &Vec<Vec<Option<bool>>>, i: usize, j: usize) -> bool {
-    let cursor = board[i][j];
-
-    if board[i].iter().all(|v| v == &cursor) {
-        return true;
+fn flip(board: &mut Vec<Vec<Option<bool>>>) {
+    for i in 0..3 {
+        for j in 0..3 {
+            board[i][j] = match board[i][j] {
+                Some(true) => Some(false),
+                Some(false) => Some(true),
+                _ => None,
+            };
+        }
     }
-
-    if (0..3).map(|k| board[k][j]).all(|v| v == cursor) {
-        return true;
-    }
-
-    if (0..3).map(|k| board[k][k]).all(|v| v == cursor) {
-        return true;
-    }
-
-    if (0..3).map(|k| board[k][2 - k]).all(|v| v == cursor) {
-        return true;
-    }
-
-    false
 }
 
-fn dfs(
-    is_takahashi: bool,
-    board: &mut Vec<Vec<Option<bool>>>,
-    a: &Vec<Vec<i64>>,
-    cache: &mut HashMap<usize, bool>,
-    takahashi: i64,
-    aoki: i64,
-) -> bool {
-    match cache.get(&hash(board)) {
-        Some(is_takahashi) => return *is_takahashi,
+fn dfs(board: &mut Vec<Vec<Option<bool>>>, a: &Vec<Vec<i64>>) -> bool {
+    match is_win(board, a) {
+        Some(true) => return true,
+        Some(false) => return false,
         _ => (),
     };
-
-    let mut did_try = false;
-    let mut can_win = false;
 
     for i in 0..3 {
         for j in 0..3 {
@@ -84,27 +74,19 @@ fn dfs(
                 continue;
             }
 
-            did_try = true;
+            board[i][j] = Some(true);
 
-            board[i][j] = Some(is_takahashi);
-
-            if is_win(board, i, j) {
-                cache.insert(hash(&board), is_takahashi);
-                return is_takahashi;
-            };
-
-            let result = dfs(!is_takahashi, board, a, cache, takahashi + if is_takahashi { a[i][j]} else { 0}, aoki + if !is_takahashi { a[i][j]} else { 0});
-
-            can_win = can_win || result;
+            flip(board);
+            let result = dfs(board, a);
+            flip(board);
 
             board[i][j] = None;
+
+            if !result {
+                return true;
+            }
         }
     }
 
-    if !did_try {
-        cache.insert(hash(&board), takahashi > aoki); // 同点どうなるの？
-        return takahashi > aoki;
-    }
-
-    can_win
+    false
 }

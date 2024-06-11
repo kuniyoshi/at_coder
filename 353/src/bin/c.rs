7034ae86eca1c3d98ff6cba58b139b7907d6bf64
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-06-11 20:52:14 +0900

    353.c: wip

--- 353/src/bin/c.rs
+++ 353/src/bin/c.rs
@@ -1,24 +1,51 @@
 fn main() {
     proconio::input! {
         n: usize,
-        a: [usize; n],
+        mut a: [usize; n],
     };
 
-    let mut acc = Vec::new();
-    acc.push(0);
-
-    for i in 0..n {
-        let last = acc.last().unwrap();
-        acc.push(last + a[i])
-    }
+    a.sort();
 
     let base = 100_000_000;
     let mut total = 0;
+    let mut overs = 0;
 
-    for i in 0..(n - 1) {
-        let addition = a[i] * (n - i - 1) + acc.last().unwrap() - acc[i + 1];
-        total += addition % base;
+    for a_value in &a {
+        total += a_value * (n - 1);
+        let bin_a = bin(&a, base - a_value);
+        let bin_b = bin(&a, a_value + 1);
+
+        if bin_a > 0 && bin_a > bin_b {
+            overs += bin_a - bin_b;
+        }
+
+        #[cfg(debug_assertions)]
+        eprintln!("{:?}", (a_value, bin_a, bin_b));
     }
 
-    println!("{}", total);
+    println!("{}", total - overs * base);
+}
+
+fn bin(a: &Vec<usize>, value: usize) -> usize {
+    if a.first().unwrap() >= &value {
+        return a.len();
+    }
+
+    if a.last().unwrap() < &value {
+        return 0;
+    }
+
+    let mut l = 0;
+    let mut r = a.len() - 1;
+
+    while r - l > 1 {
+        let m = (l + r) / 2;
+        if a[m] >= value {
+            r = m;
+        } else {
+            l = m;
+        }
+    }
+
+    a.len() - r
 }

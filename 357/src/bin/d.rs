b9eb3a5aed71092a657efe659207160a75d3f8d0
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-06-27 20:31:53 +0900

    357.d: wip

--- 357/src/bin/d.rs
+++ 357/src/bin/d.rs
@@ -3,13 +3,19 @@ fn main() {
         n: usize,
     };
 
+    // for i in 0..5 {
+    //     #[cfg(debug_assertions)]
+    //     eprintln!("{:?}", (i, f(n, i)));
+    // }
+
     let unit = 998244353;
 
     let mut result = 0;
 
+    // f(5, 2) + f(5, 0)
     for i in 0..64 {
         if (n & (1 << i)) > 0 {
-            result += f(result, i, n);
+            result += f(n, i);
             result %= unit;
         }
     }
@@ -17,8 +23,16 @@ fn main() {
     println!("{}", result);
 }
 
-fn f(current: usize, i: usize, n: usize) -> usize {
+// f(5, 0) -> 5
+// f(5, 1) -> 55
+// f(5, 2) -> 5555
+// f(5, 3) -> 55555555
+fn f(n: usize, i: usize) -> usize {
+    if i == 0 {
+        return n;
+    }
     let unit = 998244353;
-    let mut result = current * (10_u64.pow(current.to_string().len() as u32) as usize) % unit;
-     + current f(i - 1, n)
+    let r = f(n, i - 1) % unit;
+
+    (r * 10_u64.pow(r.to_string().len() as u32) as usize + r) % unit
 }

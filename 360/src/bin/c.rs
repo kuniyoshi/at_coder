a00e38783676bcfb292988a3d6a4ef1e28df4ba1
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-07-20 15:42:14 +0900

    360.c: tidy up

--- 360/src/bin/c.rs
+++ 360/src/bin/c.rs
@@ -11,20 +11,12 @@ fn main() {
         boxes[a[i] - 1].push(i);
     }
 
-    let mut total = 0;
-
-    for i in 0..n {
-        if boxes[i].len() <= 1 {
-            continue;
-        }
-
-        let mut weights: Vec<_> = boxes[i].iter().map(|j| w[*j]).collect();
-        weights.sort();
+    let total = boxes.iter().map(|baggages| {
+        let weights: Vec<_> = baggages.iter().map(|i| w[*i]).collect();
         let sum = weights.iter().sum::<u64>();
-        let max = weights.iter().last().unwrap_or(&0);
-
-        total += sum - max;
-    }
+        let max = weights.iter().max().unwrap_or(&0);
+        sum - max
+    }).sum::<u64>();
 
     println!("{}", total);
 }

757a951ff2d516e0b3607e30ae4e00c051c56d06
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-08-27 20:51:47 +0900

    363.e: wip

--- 363/src/bin/e.rs
+++ 363/src/bin/e.rs
@@ -1,3 +1,15 @@
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, VecDeque},
+};
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 fn main() {
     proconio::input! {
         h: usize,
@@ -6,35 +18,81 @@ fn main() {
         a: [[u64; w]; h],
     };
 
-    let mut hights = Vec::new();
+    let mut remains = BinaryHeap::new();
+    let mut unders = vec![0; y + 1];
+    let mut is_under = vec![vec![false; w]; h];
 
     for i in 0..h {
         for j in 0..w {
-            hights.push(
-                min(&[
-                    a.get(i.wrapping_sub(1))
-                        .and_then(|v| v.get(j.wrapping_sub(1)).copied()),
-                    a.get(i.wrapping_sub(1)).and_then(|v| v.get(j).copied()),
-                    a.get(i.wrapping_sub(1)).and_then(|v| v.get(j + 1).copied()),
-                    a.get(i).and_then(|v| v.get(j.wrapping_sub(1)).copied()),
-                    a.get(i).and_then(|v| v.get(j).copied()),
-                    a.get(i).and_then(|v| v.get(j + 1).copied()),
-                    a.get(i + 1).and_then(|v| v.get(j.wrapping_sub(1)).copied()),
-                    a.get(i + 1).and_then(|v| v.get(j).copied()),
-                    a.get(i + 1).and_then(|v| v.get(j + 1).copied()),
-                ])
-                .unwrap_or(0),
-            );
+            remains.push((Reverse(&a[i][j]), (i, j)));
+        }
+    }
+
+    let directions = [Direction::Up, Direction::Down, Direction::Left, Direction::Right];
+
+    while let Some((Reverse(&border), (base_i, base_j))) = remains.pop() {
+        if is_under[base_i][base_j] {
+            continue;
+        }
+
+        #[cfg(debug_assertions)]
+        eprintln!("{:?}", ("border", border));
+
+        let mut queue = VecDeque::new();
+
+        queue.push_back((base_i, base_j));
+
+        while let Some((i, j)) = queue.pop_front() {
+            if is_under[i][j] {
+                continue;
+            }
+
+            is_under[i][j] = true;
+
+            if border < unders.len() as u64 {
+                unders[border as usize] += 1;
+            }
+
+            for direction in &directions {
+                match coord(i, j, direction) {
+                    Some((next_i, next_j)) => {
+                        if is_under_already(&is_under, next_i, next_j) {
+                            continue;
+                        }
+
+                        if is_under_new(&a, &border, next_i, next_j) {
+                            queue.push_back((next_i, next_j))
+                        }
+                    },
+                    _ => continue,
+                };
+            }
         }
     }
 
     #[cfg(debug_assertions)]
-    eprintln!("{:?}", hights);
+    eprintln!("{:?}", unders);
 
-    // #[cfg(debug_assertions)]
-    // eprintln!("{:?}", a);
+    #[cfg(debug_assertions)]
+    eprintln!("{:?}", is_under);
 }
 
-fn min(values: &[Option<u64>]) -> Option<u64> {
-    values.iter().filter_map(|&x| x).min()
+fn is_under_new(a: &Vec<Vec<u64>>, border: &u64, i: usize, j: usize) -> bool {
+    match a.get(i).and_then(|vec| vec.get(j)) {
+        Some(value) => value <= border,
+        _ => false,
+    }
+}
+
+fn is_under_already(a: &Vec<Vec<bool>>, i: usize, j: usize) -> bool {
+    *a.get(i).and_then(|vec| vec.get(j)).unwrap_or(&false)
+}
+
+fn coord(i: usize, j: usize, direction: &Direction) -> Option<(usize, usize)> {
+    match direction {
+        Direction::Up => i.checked_sub(1).and_then(|checked_i| Some((checked_i, j))),
+        Direction::Down => Some((i + 1, j)),
+        Direction::Left => j.checked_sub(1).and_then(|checked_j| Some((i, checked_j))),
+        Direction::Right => Some((i, j + 1)),
+    }
 }

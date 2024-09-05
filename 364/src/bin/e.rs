dd6080d5695d439e04c3429386d2ac59056b07d9
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2024-09-05 20:11:09 +0900

    364.e: wip

--- 364/src/bin/e.rs
+++ 364/src/bin/e.rs
@@ -9,43 +9,51 @@ fn main() {
     #[cfg(debug_assertions)]
     eprintln!("{:?}", (n, x, y, &dishes));
 
-    let border = 10_010;
+    let limit = (dishes.iter().map(|(a, _)| *a).max().unwrap_or(0) + x.max(y) + 1) as usize;
 
-    let mut dp = vec![None; border * border];
-    dp[0] = Some(0);
+    let mut dp = vec![vec![vec![None; limit]; limit]; n + 1];
+    dp[0][0][0] = Some(0);
 
-    for &(sweetness, saltiness) in &dishes {
-        for i in (0..dp.len()).rev() {
-            match i.checked_sub((sweetness * border as u64 + saltiness) as usize) {
-                None => continue,
-                Some(previous) => {
-                    match dp[previous] {
-                        None => continue,
-                        Some(count) => {
-                            match dp[i] {
-                                None => dp[i] = Some(1),
-                                Some(current_count) => dp[i] = Some(current_count.max(count + 1)),
-                            }
-                        }
-                    }
+    for (index, &(sweetness, saltiness)) in dishes.iter().enumerate() {
+        for i in 0..limit {
+            if i > x as usize {
+                continue;
+            }
+
+            for j in 0..limit {
+                if j > y as usize {
+                    continue;
                 }
-            };
-        }
-    }
 
-    let mut max = 0;
-
-    for (index, &count) in dp.iter().enumerate() {
-        if index > x as usize * border + y as usize {
-            continue;
-        }
-
-        match count {
-            Some(c) => max = max.max(c),
-            _ => continue,
+                if let Some(count) = dp[index][i as usize][j as usize] {
+                    dp[index + 1][i + sweetness as usize][j + saltiness as usize] = max(
+                        dp[index][i + sweetness as usize][j + saltiness as usize],
+                        count + 1,
+                    );
+                }
+            }
         }
     }
 
     #[cfg(debug_assertions)]
-    eprintln!("{:?}", max);
+    eprintln!("{:?}", dp);
+
+    let mut result = 0;
+
+    for i in 0..limit {
+        for j in 0..limit {
+            if let Some(count) = dp[dishes.len()][i][j] {
+                result = result.max(count);
+            }
+        }
+    }
+
+    println!("{}", result);
+}
+
+fn max(a: Option<u64>, b: u64) -> Option<u64> {
+    if let Some(a_value) = a {
+        return Some(a_value.max(b));
+    }
+    None
 }
